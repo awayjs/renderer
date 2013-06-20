7a829cd7fb8b67d2b45dec15dcc4edaac51be22c
Karim Beyrouti <karim@kurst.co.uk>
2013-06-20 13:47:51 +0100

    Added core data classes ( needs interfaces to compile - working on these )

---
+++ src/away3d/core/data/EntityListItem.ts
@@ -0,0 +1,15 @@
+module away3d.core.data
+{
+
+	//import away3d.entities.Entity;
+
+	export class EntityListItem
+	{
+		public entity:Entity;
+		public next:EntityListItem;
+		
+		constructor() {
+			
+		}
+	}
+}
---
+++ src/away3d/core/data/RenderableListItemPool.ts
@@ -0,0 +1,36 @@
+/// <reference path="RenderableListItem.ts" />
+module away3d.core.data
+{
+	export class RenderableListItemPool
+	{
+		private var _pool : away3d.core.data.RenderableListItem[];
+		private var _index : number;//int
+		private var _poolSize : number;//int
+
+		constructor()
+		{
+			this._pool = new Array<away3d.core.data.RenderableListItem>();
+		}
+
+		public getItem() : RenderableListItem
+		{
+			if (this._index == this._poolSize) {
+				var item : RenderableListItem = new RenderableListItem();
+                this._pool[this._index++] = item;
+				++this._poolSize;
+				return item;
+			}
+			else return this._pool[this._index++];
+		}
+
+		public freeAll() : void
+		{
+            this._index = 0;
+		}
+
+		public dispose() : void
+		{
+            this._pool.length = 0;
+		}
+	}
+}
