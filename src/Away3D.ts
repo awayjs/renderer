20534bdc7d739652211b79a9657bd1a2b014d062
Karim Beyrouti <karim@kurst.co.uk>
2013-06-20 12:00:18 +0100

    Updates for ts 0.9.0.0

--- src/Away3D.ts
+++ src/Away3D.ts
@@ -1,6 +1,8 @@
 //--------------------------------------------------------------------------------------------
-// Note: WebStorm test compile ( not commiting the export JS yet)
+// Note: WebStorm test compile ( not commiting the export JS yet):
+//
 //  --sourcemap $ProjectFileDir$/src/Away3D.ts --target ES5  --comments --declaration --out $ProjectFileDir$/bin-debug/js/
+//
 //--------------------------------------------------------------------------------------------
 
 /// <reference path="away3d/core/math/Orientation3D.ts" />
@@ -11,3 +13,51 @@
 /// <reference path="away3d/core/math/Plane3D.ts" />
 /// <reference path="away3d/core/math/PlaneClassification.ts" />
 /// <reference path="away3d/core/math/Quaternion.ts" />
+
+module test{
+
+    export class CoreMathTest {
+
+        private va : away3d.core.math.Vector3D = new away3d.core.math.Vector3D(1,2,3,4);
+        private vb : away3d.core.math.Vector3D = new away3d.core.math.Vector3D(4,3,2,1);
+
+        private ma : away3d.core.math.Matrix3D  = new away3d.core.math.Matrix3D();
+        private mb : away3d.core.math.Matrix3D  = new away3d.core.math.Matrix3D();
+
+        private qa : away3d.core.math.Quaternion= new away3d.core.math.Quaternion();
+        private qb : away3d.core.math.Quaternion= new away3d.core.math.Quaternion();
+
+        public start() : void {
+
+            this.va.subtract( this.vb );
+            console.log( this.va , this.va.toString() );
+
+        }
+
+
+    }
+
+}
+
+var coreMathTest : test.CoreMathTest;
+
+/*
+ */
+function onWindowLoad( event ) {
+
+    coreMathTest = new test.CoreMathTest();
+    coreMathTest.start();
+
+}
+
+/*
+ */
+function onWindowError( event ) {
+
+
+}
+
+window.onload  = onWindowLoad;
+window.onerror = onWindowError;
+
+
