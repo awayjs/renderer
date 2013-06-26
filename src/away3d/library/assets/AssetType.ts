bafe8f9f2fa824eae3deaca6ec67b754cb7dfc89
Karim Beyrouti <karim@kurst.co.uk>
2013-06-26 14:01:17 +0100

    Added AssetType

---
+++ src/away3d/library/assets/AssetType.ts
@@ -0,0 +1,27 @@
+module away3d.library.assets
+{
+	export class AssetType
+	{
+		public static ENTITY : string = 'entity';
+		public static SKYBOX : string = 'skybox';
+		public static CAMERA : string = 'camera';
+		public static SEGMENT_SET : string = 'segmentSet';
+		public static MESH : string = 'mesh';
+		public static GEOMETRY : string = 'geometry';
+		public static SKELETON : string = 'skeleton';
+		public static AssetType.SKELETON_POSE : string = 'skeletonPose';
+		public static CONTAINER : string = 'container';
+		public static TEXTURE : string = 'texture';;
+		public static AssetType.TEXTURE_PROJECTOR : string = 'textureProjector';
+		public static MATERIAL : string = 'material';
+		public static ANIMATION_SET : string = 'animationSet';
+		public static ANIMATION_STATE : string = 'animationState';
+		public static ANIMATION_NODE : string = 'animationNode';
+		public static ANIMATOR : string = 'animator';
+		public static STATE_TRANSITION : string = 'stateTransition';
+		public static LIGHT : string = 'light';
+		public static AssetType.LIGHT_PICKER : string = 'lightPicker';
+		public static SHADOW_MAP_METHOD : string = 'shadowMapMethod';
+		public static EFFECTS_METHOD : string = 'effectsMethod';
+	}
+}
