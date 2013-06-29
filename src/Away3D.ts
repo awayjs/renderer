1f8df501ce4546ea6a860afc589b51aa28a7abe0
Gary Paluk <garypaluk@googlemail.com>
2013-06-29 04:21:26 +0100

    Initial new package structure commit

--- src/Away3D.ts
+++ src/Away3D.ts
@@ -1,89 +1,28 @@
-//--------------------------------------------------------------------------------------------
-// WebStorm compile param strings
-//--------------------------------------------------------------------------------------------
-//
-// Multiple JS export:
-//  --sourcemap $ProjectFileDir$/src/Away3D.ts --target ES5  --comments --declaration --out $ProjectFileDir$/bin-debug/js/
-//
-// Single JS export:
-//  --sourcemap $ProjectFileDir$/src/Away3D.ts --target ES5  --comments --declaration --out $ProjectFileDir$/bin-debug/js/Away3D_all.js
-//
-//--------------------------------------------------------------------------------------------
+class Greeter {
+    element: HTMLElement;
+    span: HTMLElement;
+    timerToken: number;
 
-/// <reference path="away3d/core/math/Orientation3D.ts" />
-/// <reference path="away3d/core/math/Vector3D.ts" />
-/// <reference path="away3d/core/math/MathConsts.ts" />
-/// <reference path="away3d/core/math/Matrix3DUtils.ts" />
-/// <reference path="away3d/core/math/Matrix3D.ts" />
-/// <reference path="away3d/core/math/Plane3D.ts" />
-/// <reference path="away3d/core/math/PlaneClassification.ts" />
-/// <reference path="away3d/core/math/Quaternion.ts" />
+    constructor(element: HTMLElement) {
+        this.element = element;
+        this.element.innerHTML += "The time is: ";
+        this.span = document.createElement('span');
+        this.element.appendChild(this.span);
+        this.span.innerText = new Date().toUTCString();
+    }
 
+    start() {
+        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
+    }
 
-/// <reference path="away3d/core/data/EntityListItemPool.ts" />
-/// <reference path="away3d/core/data/RenderableListItemPool.ts" />
-
-// TEST Commit...
-
-
-module test{
-
-    export class CoreMathTest {
-
-        private va : away3d.core.math.Vector3D = new away3d.core.math.Vector3D(1,2,3,4);
-        private vb : away3d.core.math.Vector3D = new away3d.core.math.Vector3D(4,3,2,1);
-
-        private ma : away3d.core.math.Matrix3D  = new away3d.core.math.Matrix3D();
-        private mb : away3d.core.math.Matrix3D  = new away3d.core.math.Matrix3D();
-
-        private qa : away3d.core.math.Quaternion= new away3d.core.math.Quaternion(1,2,3,4);
-        private qb : away3d.core.math.Quaternion= new away3d.core.math.Quaternion(1,2,3,5);
-        private qc : away3d.core.math.Quaternion= new away3d.core.math.Quaternion(11,12,13,14);
-
-        private plane3d : away3d.core.math.Plane3D= new away3d.core.math.Plane3D(.1,.5,.7,1.1 );
-
-        public start() : void {
-
-            // Quick tests to make sure it compiles
-
-            this.va.subtract( this.vb );
-            console.log( this.va , this.va.toString() );
-
-            this.qa.lerp( this.qb, this.qc,.25);
-            console.log( this.qa , this.qa.toString() );
-
-            this.plane3d.normalize();
-            console.log( this.plane3d, this.plane3d.toString() );
-
-            this.va.normalize()
-            this.plane3d.fromNormalAndPoint( this.va , this.vb )
-
-            console.log( 'Matrix3DUtils.compare:' + away3d.core.math.Matrix3DUtils.compare( this.ma , this.ma ) );
-        }
-
+    stop() {
+        clearTimeout(this.timerToken);
     }
 
 }
 
-var coreMathTest : test.CoreMathTest;
-
-/*
- */
-function onWindowLoad( event ) {
-
-    coreMathTest = new test.CoreMathTest();
-    coreMathTest.start();
-
-}
-
-/*
- */
-function onWindowError( event ) {
-
-
-}
-
-window.onload  = onWindowLoad;
-window.onerror = onWindowError;
-
-
+window.onload = () => {
+    var el = document.getElementById('content');
+    var greeter = new Greeter(el);
+    greeter.start();
+};
--- src/away/geom/Vector3D.ts
+++ src/away/geom/Vector3D.ts
@@ -1,39 +1,37 @@
 /**
  * @author Gary Paluk
- * @created 6/19/13
- * @module away3d.core.math
+ * @created 6/29/13
+ * @module away.geom
  */
-module away3d.core.math {
+module away.geom {
 
-    export class Vector3D
-    {
+    export class Vector3D {
 
         /**
          * The first element of a Vector3D object, such as the x coordinate of a point in the three-dimensional space.
          */
-        public x:number;
+        public x: number;
 
         /*
          *The second element of a Vector3D object, such as the y coordinate of a point in the three-dimensional space.
          */
-        public y:number;
+        public y: number;
 
         /**
          * The third element of a Vector3D object, such as the y coordinate of a point in the three-dimensional space.
          */
-        public z:number;
+        public z: number;
 
         /**
          * The fourth element of a Vector3D object (in addition to the x, y, and z properties) can hold data such as
          * the angle of rotation.
          */
-        public w:number;
+        public w: number;
 
         /**
          * Creates an instance of a Vector3D object.
          */
-        constructor( x:number = 0, y:number = 0, z: number = 0, w: number = 0)
-        {
+        constructor(x: number = 0, y: number = 0, z: number = 0, w: number = 0) {
             this.x = x;
             this.y = y;
             this.z = z;
@@ -45,8 +43,7 @@ module away3d.core.math {
          * x, y, and z coordinates.
          * @returns The length of the Vector3D
          */
-        public get length():number
-        {
+        public get length(): number {
             return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
         }
 
@@ -55,8 +52,7 @@ module away3d.core.math {
          * properties.
          * @returns The squared length of the vector
          */
-        public get lengthSquared():number
-        {
+        public get lengthSquared(): number {
             return (this.x * this.x + this.y * this.y + this.z + this.z);
         }
 
@@ -64,32 +60,28 @@ module away3d.core.math {
          * Adds the value of the x, y, and z elements of the current Vector3D object to the values of the x, y, and z
          * elements of another Vector3D object.
          */
-        public add(a:Vector3D):Vector3D
-        {
-            return new Vector3D( this.x + a.x, this.y + a.y, this.z + a.z, this.w + a.w)
+        public add(a: Vector3D): Vector3D {
+            return new Vector3D(this.x + a.x, this.y + a.y, this.z + a.z, this.w + a.w)
         }
 
         /**
          * [static] Returns the angle in radians between two vectors.
          */
-        static angleBetween(a:Vector3D, b:Vector3D):number
-        {
-            return Math.acos(a.dotProduct(b) /(a.length* b.length));
+        static angleBetween(a: Vector3D, b: Vector3D): number {
+            return Math.acos(a.dotProduct(b) / (a.length * b.length));
         }
 
         /**
          * Returns a new Vector3D object that is an exact copy of the current Vector3D object.
          */
-        public clone():Vector3D
-        {
+        public clone(): Vector3D {
             return new Vector3D(this.x, this.y, this.z, this.w);
         }
 
         /**
          * Copies all of vector data from the source Vector3D object into the calling Vector3D object.
          */
-        public copyFrom(src:Vector3D):Vector3D
-        {
+        public copyFrom(src: Vector3D): Vector3D {
             return new Vector3D(src.x, src.y, src.z, src.w);
         }
 
@@ -97,19 +89,17 @@ module away3d.core.math {
          * Returns a new Vector3D object that is perpendicular (at a right angle) to the current Vector3D and another
          * Vector3D object.
          */
-        public crossProduct(a:Vector3D):Vector3D
-        {
-            return new Vector3D( this.y * a.z - this.z * a.y,
-                                 this.z * a.x - this.x * a.z,
-                                 this.x * a.y - this.y * a.x );
+        public crossProduct(a: Vector3D): Vector3D {
+            return new Vector3D(this.y * a.z - this.z * a.y,
+                this.z * a.x - this.x * a.z,
+                this.x * a.y - this.y * a.x);
         }
 
         /**
          * Decrements the value of the x, y, and z elements of the current Vector3D object by the values of the x, y,
          * and z elements of specified Vector3D object.
          */
-        public decrementBy(a:Vector3D)
-        {
+        public decrementBy(a: Vector3D) {
             this.x -= a.x;
             this.y -= a.y;
             this.z -= a.z;
@@ -118,20 +108,18 @@ module away3d.core.math {
         /**
          * [static] Returns the distance between two Vector3D objects.
          */
-        static distance(pt1:Vector3D, pt2:Vector3D):number
-        {
-            var dx:number = (pt1.x - pt2.x) * (pt1.x - pt2.x);
-            var dy:number = (pt1.y - pt2.y) * (pt1.y - pt2.y);
-            var dz:number = (pt1.z - pt2.z) * (pt1.z - pt2.z);
-            return Math.sqrt(dx + dy + dz);
+        static distance(pt1: Vector3D, pt2: Vector3D): number {
+            var x: number = (pt1.x - pt2.x);
+            var y: number = (pt1.y - pt2.y);
+            var z: number = (pt1.z - pt2.z);
+            return Math.sqrt(x*x + y*y + z*z);
         }
 
         /**
          * If the current Vector3D object and the one specified as the parameter are unit vertices, this method returns
          * the cosine of the angle between the two vertices.
          */
-        public dotProduct(a:Vector3D)
-        {
+        public dotProduct(a: Vector3D) {
             return this.x * a.x + this.y * a.y + this.z * a.z;
         }
 
@@ -139,21 +127,15 @@ module away3d.core.math {
          * Determines whether two Vector3D objects are equal by comparing the x, y, and z elements of the current
          * Vector3D object with a specified Vector3D object.
          */
-        public equals(cmp:Vector3D, allFour:boolean = false):boolean
-        {
-            if( !allFour )
-            {
-                return (this.x == cmp.x && this.y == cmp.y && this.z == cmp.z);
-            }
-            return (this.x == cmp.x && this.y == cmp.y && this.z == cmp.z && this.w == cmp.w);
+        public equals(cmp: Vector3D, allFour: boolean = false): boolean {
+            return (this.x == cmp.x && this.y == cmp.y && this.z == cmp.z && (!allFour || this.w == cmp.w ));
         }
 
         /**
          * Increments the value of the x, y, and z elements of the current Vector3D object by the values of the x, y,
          * and z elements of a specified Vector3D object.
          */
-        public incrementBy(a:Vector3D)
-        {
+        public incrementBy(a: Vector3D) {
             this.x += a.x;
             this.y += a.y;
             this.z += a.z;
@@ -163,22 +145,17 @@ module away3d.core.math {
          * Compares the elements of the current Vector3D object with the elements of a specified Vector3D object to
          * determine whether they are nearly equal.
          */
-        public nearEquals(cmp:Vector3D, epsilon:number, allFour:boolean = true):boolean
-        {
-            if((Math.abs(this.x - cmp.x) < epsilon )
-             ||(Math.abs(this.y - cmp.y) < epsilon )
-             ||(Math.abs(this.z - cmp.z) < epsilon ))
-            {
-                return false;
-            }
-            return Math.abs(this.w - cmp.w) > epsilon;
+        public nearEquals(cmp: Vector3D, epsilon: number, allFour: boolean = true): boolean {
+            return ((Math.abs(this.x - cmp.x) < epsilon)
+                 && (Math.abs(this.y - cmp.y) < epsilon)
+                 && (Math.abs(this.z - cmp.z) < epsilon)
+                 && (!allFour || Math.abs(this.w - cmp.w) < epsilon));
         }
 
         /**
          * Sets the current Vector3D object to its inverse.
          */
-        public negate():void
-        {
+        public negate(): void {
             this.x = -this.x;
             this.y = -this.y;
             this.z = -this.z;
@@ -188,10 +165,9 @@ module away3d.core.math {
          * Converts a Vector3D object to a unit vector by dividing the first three elements (x, y, z) by the length of
          * the vector.
          */
-        public normalize():void
-        {
+        public normalize(): void {
             var invLength = 1 / this.length;
-            if(invLength != 0)
+            if (invLength != 0)
             {
                 this.x *= invLength;
                 this.y *= invLength;
@@ -205,8 +181,7 @@ module away3d.core.math {
          * Divides the value of the x, y, and z properties of the current Vector3D object by the value of its w
          * property.
          */
-        public project():void
-        {
+        public project(): void {
             this.x /= this.w;
             this.y /= this.w;
             this.z /= this.w;
@@ -215,8 +190,7 @@ module away3d.core.math {
         /**
          * Scales the current Vector3D object by a scalar, a magnitude.
          */
-        public scaleBy(s:number):void
-        {
+        public scaleBy(s: number): void {
             this.x *= s;
             this.y *= s;
             this.z *= s;
@@ -225,8 +199,7 @@ module away3d.core.math {
         /**
          * Sets the members of Vector3D to the specified values
          */
-        public setTo(xa:number, ya:number, za:number):void
-        {
+        public setTo(xa: number, ya: number, za: number): void {
             this.x = xa;
             this.y = ya;
             this.z = za;
@@ -236,16 +209,14 @@ module away3d.core.math {
          * Subtracts the value of the x, y, and z elements of the current Vector3D object from the values of the x, y,
          * and z elements of another Vector3D object.
          */
-        public subtract(a:Vector3D):Vector3D
-        {
-            return new Vector3D( this.x - a.x,this.y - a.y, this.z - a.z);
+        public subtract(a: Vector3D): Vector3D {
+            return new Vector3D(this.x - a.x, this.y - a.y, this.z - a.z);
         }
 
         /**
          * Returns a string representation of the current Vector3D object.
          */
-        public toString():string
-        {
+        public toString(): string {
             return "[Vector3D] (x:" + this.x + " ,y:" + this.y + ", z" + this.z + ", w:" + this.w + ")";
         }
 
