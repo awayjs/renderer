6998f4fb8dcbb26a086667acce20b61dd8fbae7e
Gary Paluk <gary@plugin.io>
2013-07-01 01:44:26 +0100

    Removed VS project releated files

---
+++ src/App.ts
@@ -0,0 +1,15 @@
+/**
+ * ...
+ * @author Gary Paluk - http://www.plugin.io
+ */
+class App 
+{
+	
+	public startup():void 
+	{
+		alert("Hello World!");
+	}
+	
+}
+var main:App = new App();
+main.startup();
