a8a634a4859d95d1c64130280bdaf9d1792fc193
Karim Beyrouti <karim@kurst.co.uk>
2013-07-03 12:21:57 +0100

    URLDataFormat - initial implementation

---
+++ src/away/net/URLLoaderDataFormat.ts
@@ -0,0 +1,21 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: karimbeyrouti
+ * Date: 03/07/2013
+ * Time: 11:29
+ * To change this template use File | Settings | File Templates.
+ */
+
+module away.net
+{
+
+    export class URLLoaderDataFormat
+    {
+
+        public static BINARY    : string = 'binary';
+        public static TEXT      : string = 'text';
+        public static VARIABLES : string = 'variables';
+
+    }
+
+}
