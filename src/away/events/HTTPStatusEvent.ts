a0e6aec052db046e6a331e0037df5b8da782c603
Karim Beyrouti <karim@kurst.co.uk>
2013-07-03 15:06:48 +0100

    Created HTTPStatusEvent

---
+++ src/away/events/HTTPStatusEvent.ts
@@ -0,0 +1,21 @@
+///<reference path="Event.ts" />
+
+module away.events
+{	
+	export class HTTPStatusEvent extends away.events.Event
+	{
+		
+		//public static INITIALIZE_SUCCESS: string = "initializeSuccess";
+		//public static INITIALIZE_FAILED: string = "initializeFailed";
+
+        public static HTTP_STATUS: string = "HTTPStatusEvent_HTTP_STATUS";
+
+        public status : number;
+		
+		constructor(type:string, message:string = "")
+		{
+			super(type);
+
+		}
+	}
+}
