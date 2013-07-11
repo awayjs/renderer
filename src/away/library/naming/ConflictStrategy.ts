fc6553c41c6c9630e78f67a593340eb71574c1ec
Karim Beyrouti <karim@kurst.co.uk>
2013-07-11 09:17:03 +0100

    ConflictStrategy - Implemented

--- src/away/library/naming/ConflictStrategy.ts
+++ src/away/library/naming/ConflictStrategy.ts
@@ -1,3 +1,7 @@
+///<reference path="NumSuffixConflictStrategy.ts" />
+///<reference path="IgnoreConflictStrategy.ts" />
+///<reference path="ErrorConflictStrategy.ts" />
+
 module away.library
 {
 	
@@ -18,21 +22,21 @@ module away.library
 		 * Specifies that in case of a naming conflict, one of the assets will be renamed and
 		 * a numeric suffix appended to the base name.
 		 */
-		public static APPEND_NUM_SUFFIX:ConflictStrategyBase = new NumSuffixConflictStrategy();
+		public static APPEND_NUM_SUFFIX:ConflictStrategyBase = new away.library.NumSuffixConflictStrategy();
 		
 		/**
 		 * Specifies that naming conflicts should be ignored. This is not recommended in most
 		 * cases, unless it can be 100% guaranteed that the application does not cause naming
 		 * conflicts in the library (i.e. when an app-level system is in place to prevent this.)
 		 */
-		public static IGNORE:ConflictStrategyBase = new IgnoreConflictStrategy();
+		public static IGNORE:ConflictStrategyBase = new away.library.IgnoreConflictStrategy();
 		
 		/**
 		 * Specifies that an error should be thrown if a naming conflict is discovered. Use this
 		 * to be 100% sure that naming conflicts never occur unnoticed, and when it's undesirable
 		 * to have the library automatically rename assets to avoid such conflicts.
 		 */
-		public static THROW_ERROR:ConflictStrategyBase = new ErrorConflictStrategy();
+		public static THROW_ERROR:ConflictStrategyBase = new away.library.ErrorConflictStrategy();
 	
 	}
 }
