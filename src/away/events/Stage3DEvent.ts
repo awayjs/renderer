41cc7f2a3a33b0aceffa3a2bd0aa755f2e6b03d0
Karim Beyrouti <karim@kurst.co.uk>
2013-07-15 15:25:09 +0100

    Stage3DEvent - intial commit

---
+++ src/away/events/Stage3DEvent.ts
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+module away.events
+{
+	//import flash.events.Event;
+	
+	export class Stage3DEvent extends Event
+	{
+		public static CONTEXT3D_CREATED:string = "Context3DCreated";
+		public static CONTEXT3D_DISPOSED:string = "Context3DDisposed";
+		public static CONTEXT3D_RECREATED:string = "Context3DRecreated";
+		public static VIEWPORT_UPDATED:string = "ViewportUpdated";
+		
+		constructor(type:string )//, bubbles:boolean = false, cancelable:boolean = false)
+		{
+			super( type );//, bubbles, cancelable);
+		}
+	}
+}
