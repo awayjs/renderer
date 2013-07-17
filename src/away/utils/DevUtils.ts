2a244e100d9c729211ffba37a43587dc82257039
Karim Beyrouti <karim@kurst.co.uk>
2013-07-17 09:24:03 +0100

    added throw error flag

--- src/away/utils/DevUtils.ts
+++ src/away/utils/DevUtils.ts
@@ -7,15 +7,16 @@ module away.utils
     export class Dev
     {
 
-        public static DEBUG     : boolean = true;
-        public static LOG       : boolean = true;
+        public static THROW_ERROR   : boolean = true;
+        public static DEBUG         : boolean = true;
+        public static LOG           : boolean = true;
 
         public static throwPIR( clss : string , fnc : string , msg : string )
         {
 
             Dev.log( clss , fnc , msg );
 
-            if ( Dev.DEBUG )
+            if ( Dev.THROW_ERROR )
             {
 
                 throw new away.errors.PartialImplementationError( clss + '.' + fnc + ': ' +  msg );
