fb79b113df17119809d382cdc18d3645c1011179
Gary Paluk <gary@plugin.io>
2013-07-17 17:19:56 +0100

    Added PerspectiveOffCenterLens

--- src/away/_definitions.ts
+++ src/away/_definitions.ts
@@ -45,6 +45,7 @@
 ///<reference path="cameras/lenses/FreeMatrixLens.ts" />
 ///<reference path="cameras/lenses/OrthographicLens.ts" />
 ///<reference path="cameras/lenses/OrthographicOffCenterLens.ts" />
+///<reference path="cameras/lenses/PerspectiveOffCenterLens.ts" />
 ///<reference path="events/CameraEvent.ts" />
 ///<reference path="bounds/NullBounds.ts" />
 ///<reference path="containers/ObjectContainer3D.ts" />
