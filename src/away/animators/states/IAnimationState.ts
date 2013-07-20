14bb4eb6a4d3993f51ddceba7c3b946d5071380f
Karim Beyrouti <karim@kurst.co.uk>
2013-07-20 23:18:41 +0100

    IAnimationState - intial commit

---
+++ src/away/animators/states/IAnimationState.ts
@@ -0,0 +1,22 @@
+///<reference path="../../_definitions.ts"/>
+
+module away.animators
+{
+
+	interface IAnimationState
+	{
+
+		positionDelta():away.geom.Vector3D; // GET
+
+		offset(startTime:number);
+
+		update(time:number);
+		
+		/**
+		 * Sets the animation phase of the node.
+		 *
+		 * @param value The phase value to use. 0 represents the beginning of an animation clip, 1 represents the end.
+		 */
+		phase(value:number);
+	}
+}
