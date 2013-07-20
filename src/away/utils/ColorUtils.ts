67e3c357ddd38c0aa56238630ac5784ea2552474
Karim Beyrouti <karim@kurst.co.uk>
2013-07-20 12:24:34 +0100

    ColorUtils - initial implementation

---
+++ src/away/utils/ColorUtils.ts
@@ -0,0 +1,36 @@
+module away.utils
+{
+    export class ColorUtils
+    {
+
+        public static float32ColorToARGB( float32Color : number ) : Array
+        {
+
+            var a       : number = ( float32Color & 0xff000000 ) >>> 24
+            var r       : number = ( float32Color & 0xff0000 ) >>> 16;
+            var g       : number = ( float32Color & 0xff00 ) >>> 8;
+            var b       : number = float32Color & 0xff;
+            var result  : Array  = [ a, r , g , b ];
+
+            return result;
+
+        }
+
+        private static componentToHex( c : number ) : string
+        {
+
+            var hex = c.toString(16);
+            return hex.length == 1 ? "0" + hex : hex;
+
+        }
+
+        public static ARGBToHexString( argb : Array ) : string
+        {
+
+            return "#" + ColorUtils.componentToHex( argb[1] ) + ColorUtils.componentToHex( argb[2] ) + ColorUtils.componentToHex( argb[3] );
+
+        }
+
+
+    }
+}
