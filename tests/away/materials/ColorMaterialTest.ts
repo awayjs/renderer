0b58c27eb4faa259c1b3cb2fe4c5db28ada329cd
Karim Beyrouti <karim@kurst.co.uk>
2013-07-24 16:58:59 +0100

    ColorMaterial test

---
+++ tests/away/materials/ColorMaterialTest.ts
@@ -0,0 +1,54 @@
+///<reference path="../../../src/away/_definitions.ts" />
+
+//------------------------------------------------------------------------------------------------
+// Web / PHP Storm arguments string
+//------------------------------------------------------------------------------------------------
+// --sourcemap $ProjectFileDir$/tests/away/managers/ManagersTest.ts --target ES5 --comments --out $ProjectFileDir$/tests/away/managers/ManagersTest.js
+//------------------------------------------------------------------------------------------------
+
+class MaterialsTest
+{
+
+    private cm          : away.materials.ColorMaterial;
+
+    private _stage      : away.display.Stage;
+    private _context3D  : away.display3D.Context3D;
+
+    constructor()
+    {
+
+        away.Debug.THROW_ERRORS = false;
+
+        this.cm = new away.materials.ColorMaterial();
+
+        this._stage = new away.display.Stage( 800, 600 );
+        this._stage.stage3Ds[0].addEventListener( away.events.Event.CONTEXT3D_CREATE, this.onContext3DCreateHandler, this );
+        this._stage.stage3Ds[0].requestContext();
+
+    }
+
+    private onContext3DCreateHandler( e )
+    {
+        this._stage.stage3Ds[0].removeEventListener( away.events.Event.CONTEXT3D_CREATE, this.onContext3DCreateHandler, this );
+
+        var stage3D: away.display.Stage3D = <away.display.Stage3D> e.target;
+        this._context3D = stage3D.context3D;
+
+        this.cm.specularMethod = new away.materials.BasicSpecularMethod();
+        this.cm.iUpdateMaterial( this._context3D );
+        this.cm.iRenderPass()
+        this.cm.iInvalidatePasses( null );
+
+        console.log( this.cm );
+        console.log( this.cm._pScreenPass );
+
+    }
+
+}
+
+window.onload = function ()
+{
+
+    var test : MaterialsTest = new MaterialsTest();
+
+}
