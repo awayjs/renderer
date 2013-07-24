4cc0c8efd3e6de22a91c2ec7f81287a16608833e
Karim Beyrouti <karim@kurst.co.uk>
2013-07-24 12:54:53 +0100

    ColorMaterial intial implementation

---
+++ src/away/materials/ColorMaterial.ts
@@ -0,0 +1,76 @@
+///<reference path="../_definitions.ts"/>
+
+module away.materials
+{
+
+	/**
+	 * ColorMaterial is a single-pass material that uses a flat color as the surface's diffuse reflection value.
+	 */
+	export class ColorMaterial extends away.materials.SinglePassMaterialBase
+	{
+		private _diffuseAlpha:number = 1;
+		
+		/**
+		 * Creates a new ColorMaterial object.
+		 * @param color The material's diffuse surface color.
+		 * @param alpha The material's surface alpha.
+		 */
+		constructor(color:number = 0xcccccc, alpha:number = 1)
+		{
+
+			super();
+
+			this.color = color;
+			this.alpha = alpha;
+
+		}
+		
+		/**
+		 * The alpha of the surface.
+		 */
+		public get alpha():number
+		{
+			return this._pScreenPass.diffuseMethod.diffuseAlpha;
+		}
+		
+		public set alpha(value:number)
+		{
+			if (value > 1)
+            {
+
+                value = 1;
+
+            }
+			else if (value < 0)
+            {
+
+                value = 0;
+            }
+
+			this._pScreenPass.diffuseMethod.diffuseAlpha = this._diffuseAlpha = value;
+            this._pScreenPass.preserveAlpha = this.requiresBlending;
+            this._pScreenPass.setBlendMode( this.blendMode == away.display.BlendMode.NORMAL && this.requiresBlending? away.display.BlendMode.LAYER : this.blendMode);
+		}
+		
+		/**
+		 * The diffuse reflectivity color of the surface.
+		 */
+		public get color():number
+		{
+			return this._pScreenPass.diffuseMethod.diffuseColor;
+		}
+		
+		public set color(value:number)
+		{
+            this._pScreenPass.diffuseMethod.diffuseColor = value;
+		}
+		
+		/**
+		 * @inheritDoc
+		 */
+		public get requiresBlending():boolean
+		{
+			return super.getRequiresBlending() || this._diffuseAlpha < 1;
+		}
+	}
+}
