1e16f6e85521fc965d1bbc818a74fc113b12630e
Karim Beyrouti <karim@kurst.co.uk>
2013-07-24 15:36:07 +0100

    Testing: implemented agal string building

--- src/away/materials/methods/ShadingMethodBase.ts
+++ src/away/materials/methods/ShadingMethodBase.ts
@@ -185,7 +185,7 @@ module away.materials
 
             }
 
-			return "tex " + targetReg + ", " + uvReg + ", " + inputReg + " <2d," + filter + "," + format + wrap + ">\n";
+			return "tex " + targetReg.toString() + ", " + uvReg.toString() + ", " + inputReg.toString() + " <2d," + filter + "," + format + wrap + ">\n";
 
 		}
 
@@ -208,7 +208,7 @@ module away.materials
 			else
 				filter = enableMipMaps? "nearest,mipnearest" : "nearest";
 			
-			return "tex " + targetReg + ", " + uvReg + ", " + inputReg + " <cube," + format + filter + ">\n";
+			return "tex " + targetReg.toString() + ", " + uvReg.toString() + ", " + inputReg.toString() + " <cube," + format + filter + ">\n";
 		}
 
 		/**
