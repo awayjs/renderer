ca3cb689e81369349535eaab08c881c963e0efb1
Karim Beyrouti <karim@kurst.co.uk>
2013-07-24 09:40:39 +0100

    ColorTransformMethod - initial implementation

---
+++ src/away/materials/methods/ColorTransformMethod.ts
@@ -0,0 +1,82 @@
+///<reference path="../../_definitions.ts"/>
+module away.materials
+{
+	//import away3d.arcane;
+	//import away3d.managers.Stage3DProxy;
+	//import away3d.materials.compilation.ShaderRegisterCache;
+	//import away3d.materials.compilation.ShaderRegisterElement;
+	
+	//import flash.geom.ColorTransform;
+	
+	//use namespace arcane;
+	
+	/**
+	 * ColorTransformMethod provides a shading method that changes the colour of a material analogous to a
+	 * ColorTransform object.
+	 */
+	export class ColorTransformMethod extends away.materials.EffectMethodBase
+	{
+		private _colorTransform:away.geom.ColorTransform;
+		
+		/**
+		 * Creates a new ColorTransformMethod.
+		 */
+		constructor()
+		{
+			super();
+		}
+		
+		/**
+		 * The ColorTransform object to transform the colour of the material with.
+		 */
+		public get colorTransform():away.geom.ColorTransform
+		{
+			return this._colorTransform;
+		}
+		
+		public set colorTransform(value:away.geom.ColorTransform)
+		{
+            this._colorTransform = value;
+		}
+		
+		/**
+		 * @inheritDoc
+		 */
+		public iGetFragmentCode(vo:away.materials.MethodVO, regCache:away.materials.ShaderRegisterCache, targetReg:away.materials.ShaderRegisterElement):string
+		{
+			var code:string = "";
+			var colorMultReg:away.materials.ShaderRegisterElement = regCache.getFreeFragmentConstant();
+			var colorOffsReg:away.materials.ShaderRegisterElement = regCache.getFreeFragmentConstant();
+
+			vo.fragmentConstantsIndex = colorMultReg.index*4;
+
+            //TODO: AGAL <> GLSL
+
+            /*
+			code += "mul " + targetReg + ", " + targetReg.toString() + ", " + colorMultReg + "\n" +
+				"add " + targetReg + ", " + targetReg.toString() + ", " + colorOffsReg + "\n";
+            */
+			return code;
+		}
+		
+		/**
+		 * @inheritDoc
+		 */
+		public iActivate(vo:away.materials.MethodVO, stage3DProxy:away.managers.Stage3DProxy)
+		{
+			var inv:number = 1/0xff;
+			var index:number = vo.fragmentConstantsIndex;
+			var data:number[] = vo.fragmentData;
+
+			data[index] = this._colorTransform.redMultiplier;
+			data[index + 1] = this._colorTransform.greenMultiplier;
+			data[index + 2] = this._colorTransform.blueMultiplier;
+			data[index + 3] = this._colorTransform.alphaMultiplier;
+			data[index + 4] = this._colorTransform.redOffset*inv;
+			data[index + 5] = this._colorTransform.greenOffset*inv;
+			data[index + 6] = this._colorTransform.blueOffset*inv;
+			data[index + 7] = this._colorTransform.alphaOffset*inv;
+
+		}
+	}
+}
