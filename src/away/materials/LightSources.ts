c5ec15b334ba810fc7a73a388fcd96c59a5bdbc0
Karim Beyrouti <karim@kurst.co.uk>
2013-07-24 09:44:03 +0100

    LightSources - initial implementation

---
+++ src/away/materials/LightSources.ts
@@ -0,0 +1,35 @@
+module away.materials
+{
+	
+	/**
+	 * Enumeration class for defining which lighting types affect the specific material
+	 * lighting component (diffuse and specular). This can be useful if, for example, you
+	 * want to use light probes for diffuse global lighting, but want specular reflections from
+	 * traditional light sources without those affecting the diffuse light.
+	 *
+	 * @see away3d.materials.ColorMaterial.diffuseLightSources
+	 * @see away3d.materials.ColorMaterial.specularLightSources
+	 * @see away3d.materials.TextureMaterial.diffuseLightSources
+	 * @see away3d.materials.TextureMaterial.specularLightSources
+	 */
+	export class LightSources
+	{
+		/**
+		 * Defines normal lights are to be used as the source for the lighting
+		 * component.
+		 */
+		public static LIGHTS:number = 0x01;
+		
+		/**
+		 * Defines that global lighting probes are to be used as the source for the
+		 * lighting component.
+		 */
+		public static PROBES:number = 0x02;
+		
+		/**
+		 * Defines that both normal and global lighting probes  are to be used as the
+		 * source for the lighting component. This is equivalent to LightSources.LIGHTS | LightSources.PROBES.
+		 */
+		public static ALL:number = 0x03;
+	}
+}
