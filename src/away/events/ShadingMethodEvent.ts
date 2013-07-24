07e64fdb5912c8082b8d86dfab4c9099acc04f39
Karim Beyrouti <karim@kurst.co.uk>
2013-07-24 09:35:40 +0100

    ShadingMethodEvent - Initial Implementation

---
+++ src/away/events/ShadingMethodEvent.ts
@@ -0,0 +1,19 @@
+
+///<reference path="../_definitions.ts"/>
+
+module away.events
+{
+	//import flash.events.Event;
+	
+	export class ShadingMethodEvent extends away.events.Event
+	{
+		public static SHADER_INVALIDATED:string = "ShaderInvalidated";
+		
+		constructor(type:string )
+		{
+
+			super(type );
+
+		}
+	}
+}
