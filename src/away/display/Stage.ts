bd7dc7fa0f21d70364f79e849fc80127d6bd5cac
Karim Beyrouti <karim@kurst.co.uk>
2013-07-31 13:15:20 +0100

    Stage - updated to only add canvas to document when context is requested

--- src/away/display/Stage.ts
+++ src/away/display/Stage.ts
@@ -29,17 +29,27 @@ module away.display
 			
 			this.initStage3DObjects();
 			this.resize( width, height );
+
 		}
 		
 		public resize( width:number, height:number )
 		{
 			this._stageHeight = height;
 			this._stageWidth = width;
-			
+
+            var s3d : away.display.Stage3D;
+
 			for( var i: number = 0; i <  Stage.STAGE3D_MAX_QUANTITY; ++i )
 			{
-				away.utils.CSS.setCanvasSize( this.stage3Ds[ i ].canvas, width, height );
-				away.utils.CSS.setCanvasPosition( this.stage3Ds[ i ].canvas, 0, 0, true );
+
+                s3d         = this.stage3Ds[ i ];
+                s3d.width   = width;
+                s3d.height  = height;
+                s3d.x       = 0;
+                s3d.y       = 0;
+
+				//away.utils.CSS.setCanvasSize( this.stage3Ds[ i ].canvas, width, height );
+				//away.utils.CSS.setCanvasPosition( this.stage3Ds[ i ].canvas, 0, 0, true );
 			}
 			this.dispatchEvent( new away.events.Event( away.events.Event.RESIZE ) );
 		}
@@ -56,14 +66,27 @@ module away.display
 		public initStage3DObjects()
 		{
 			this.stage3Ds = [];
+
 			for( var i: number = 0; i < Stage.STAGE3D_MAX_QUANTITY; ++i )
 			{
-				var canvas: HTMLCanvasElement = this.createHTMLCanvasElement();
-				this.addChildHTMLElement( canvas );
-				this.stage3Ds.push( new away.display.Stage3D( canvas ) );
+
+				var canvas  : HTMLCanvasElement     = this.createHTMLCanvasElement();
+                var stage3D : away.display.Stage3D  = new away.display.Stage3D( canvas );
+                    stage3D.addEventListener( away.events.Event.CONTEXT3D_CREATE , this.onContextCreated , this );
+
+				this.stage3Ds.push( stage3D );
+
 			}
+
 		}
-		
+
+        private onContextCreated(e : away.events.Event ) : void
+        {
+
+            var stage3D : away.display.Stage3D = <away.display.Stage3D> e.target;
+            this.addChildHTMLElement( stage3D.canvas );
+        }
+
 		private createHTMLCanvasElement(): HTMLCanvasElement
 		{
 			return document.createElement( "canvas" );
