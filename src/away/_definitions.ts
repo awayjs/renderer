241fca0c769a79b329f714c035f1498473e0df2e
Karim Beyrouti <karim@kurst.co.uk>
2013-08-02 19:42:29 +0100

    added comment to definitions file

--- src/away/_definitions.ts
+++ src/away/_definitions.ts
@@ -1,3 +1,18 @@
+
+/**********************************************************************************************************************************************************************************************************
+ * This file contains a reference to all the classes used in the project.
+ ********************************************************************************************************************************************************************************************************
+ *
+ * The TypeScript compiler exports classes in a non deterministic manner, as the extend functionality copies the prototype chain
+ * of one object onto another during initialisation and load (to create extensible functionality), the non deterministic nature of the
+ * compiler can result in an extend operation referencing a class that is undefined and not yet loaded - which throw an JavaScript error.
+ *
+ * This file provides the compiler with a strict order in which to export the TypeScript classes to mitigate undefined extend errors
+ *
+ * @see https://typescript.codeplex.com/workitem/1356 @see https://typescript.codeplex.com/workitem/913
+ *
+ *********************************************************************************************************************************************************************************************************/
+
 ///<reference path="../def/webgl.d.ts"/>
 ///<reference path="../def/js.d.ts"/>
 ///<reference path="events/Event.ts" />
