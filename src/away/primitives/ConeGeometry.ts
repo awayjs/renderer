b5cc955e86a1e51180310affd0915a0b9f1c2695
Karim Beyrouti <karim@kurst.co.uk>
2013-08-02 11:59:04 +0100

    ConeGeometry - initial implementation

---
+++ src/away/primitives/ConeGeometry.ts
@@ -0,0 +1,39 @@
+///<reference path="../_definitions.ts"/>
+
+module away.primitives
+{
+	
+	/**
+	 * A UV Cone primitive mesh.
+	 */
+	export class ConeGeometry extends away.primitives.CylinderGeometry
+	{
+		
+		/**
+		 * The radius of the bottom end of the cone.
+		 */
+		public get radius():number
+		{
+			return this._pBottomRadius;
+		}
+		
+		public set radius(value:number)
+		{
+			this._pBottomRadius = value;
+            this.pInvalidateGeometry();
+		}
+		
+		/**
+		 * Creates a new Cone object.
+		 * @param radius The radius of the bottom end of the cone
+		 * @param height The height of the cone
+		 * @param segmentsW Defines the number of horizontal segments that make up the cone. Defaults to 16.
+		 * @param segmentsH Defines the number of vertical segments that make up the cone. Defaults to 1.
+		 * @param yUp Defines whether the cone poles should lay on the Y-axis (true) or on the Z-axis (false).
+		 */
+		constructor(radius:number = 50, height:number = 100, segmentsW:number = 16, segmentsH:number = 1, closed:boolean = true, yUp:boolean = true)
+		{
+			super(0, radius, height, segmentsW, segmentsH, false, closed, true, yUp);
+		}
+	}
+}
