dbe9d4456833b9ee9020038dc1525e65c332e97c
Karim Beyrouti <karim@kurst.co.uk>
2013-08-06 11:07:47 +0100

    SkyBoxNode - initial implementation

---
+++ src/away/partition/SkyBoxNode.ts
@@ -0,0 +1,45 @@
+///<reference path="../_definitions.ts"/>
+
+module away.partition
+{
+
+	/**
+	 * SkyBoxNode is a space partitioning leaf node that contains a SkyBox object.
+	 */
+	export class SkyBoxNode extends away.partition.EntityNode
+	{
+		private _skyBox:away.entities.SkyBox;
+		
+		/**
+		 * Creates a new SkyBoxNode object.
+		 * @param skyBox The SkyBox to be contained in the node.
+		 */
+		constructor(skyBox:away.entities.SkyBox)
+		{
+			super(skyBox);
+			this._skyBox = skyBox;
+		}
+		
+		/**
+		 * @inheritDoc
+		 */
+		public acceptTraverser(traverser:away.traverse.PartitionTraverser)
+		{
+
+
+			if (traverser.enterNode(this)) {
+				super.acceptTraverser(traverser);
+				traverser.applySkyBox(this._skyBox);
+			}
+		}
+		
+		public isInFrustum(planes:away.math.Plane3D[], numPlanes:number):boolean
+		{
+
+			planes = planes;
+			numPlanes = numPlanes;
+
+			return true;
+		}
+	}
+}
