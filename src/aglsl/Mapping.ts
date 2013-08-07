858f811d97717d99875aa04860b837f02a9cbb8b
Karim Beyrouti <karim@kurst.co.uk>
2013-08-07 11:23:42 +0100

    Mapping - Updated / Fixed errors

--- src/aglsl/Mapping.ts
+++ src/aglsl/Mapping.ts
@@ -36,15 +36,12 @@ module aglsl
 			new OpLUT( "%dest = %cast(dot(vec4(%a),vec4(%b)));\n",		0, 	   	true, 	true, true, 	null, null, true, null, null, null ), //dp4
 			new OpLUT( "%dest = %cast(abs(%a));\n",						0, 	   	true, 	true, false, 	null, null, null, null, null, null ), //abs
 			new OpLUT( "%dest = %cast(%a * -1.0);\n",					0, 	   	true, 	true, false, 	null, null, null, null, null, null ), //neg
-			new OpLUT( "%dest = %cast(%a * -1.0);\n",					0, 	   	true, 	true, false, 	null, null, null, null, null, null ), //sat
-			
-			new OpLUT( "%dest = %cast(%a * -1.0);\n",					0, 	   	true, 	true, true, 	3, 	  3,    true, null, null, null ), //m33
-			new OpLUT( "%dest = %cast(%a * -1.0);\n",					0, 	   	true, 	true, true, 	4, 	  4,    true, null, null, null ), //m44
-			new OpLUT( "%dest = %cast(%a * -1.0);\n",					0, 	   	true, 	true, true, 	4, 	  3,    true, null, null, null ), //m43
-			
+            new OpLUT( "%dest = %cast(clamp(%a,0.0,1.0));\n",           0,      true,   true, false,    null, null, null, null, null, null ), //sat - fixed
+            new OpLUT( "%dest = %cast(dot(vec3(%a),vec3(%b)));\n",      null,   true,   true, true,     3,    3,    true, null, null, null ), //m33 - fixed
+            new OpLUT( "%dest = %cast(dot(vec4(%a),vec4(%b)));\n",      null,   true,   true, true,     4,    4,    true, null, null, null ), //m44 - fixed
+            new OpLUT( "%dest = %cast(dot(vec4(%a),vec4(%b)));\n",      null,   true,   true, true,     4,    3,    true, null, null, null ), //m43 - fixed
 			new OpLUT( "%dest = %cast(dFdx(%a));\n",					0, 	   	true, 	true, false, 	null, null, null, null, null, null ), //dFdx
 			new OpLUT( "%dest = %cast(dFdx(%a));\n",					0, 	   	true, 	true, false, 	null, null, null, null, null, null ), //dFdx
-			
 			new OpLUT( "if (float(%a)==float(%b)) {;\n",				0, 	   	false, 	true, true, 	null, null, null, true, null, null ),
 			new OpLUT( "if (float(%a)!=float(%b)) {;\n",				0, 	   	false, 	true, true, 	null, null, null, true, null, null ),
 			new OpLUT( "if (float(%a)>=float(%b)) {;\n",				0, 	   	false, 	true, true, 	null, null, null, true, null, null ),
@@ -59,7 +56,7 @@ module aglsl
 			//         s 															flags  	dest    a     b 	    mw 	  mh    ndwm  scale dm	  lod 
 			new OpLUT( "%dest = %cast(texture%texdimLod(%b,%texsize(%a)).%dm);\n",	null,  	true, 	true, true, 	null, null, null, null, true, null ),
 			new OpLUT( "if ( float(%a)<0.0 ) discard;\n",							null,  	false, 	true, false, 	null, null, null, true, null, null ),
-			new OpLUT( "%dest = %cast(texture%texdim(%b,%texsize(%a)%lod).%dm);\n",	null,  	true, 	true, true, 	null, null, null, null, null, true ),
+            new OpLUT( "%dest = %cast(texture%texdim(%b,%texsize(%a)%lod).%dm);\n", null,   true,   true, true,     null, null, true, null, true, true ), // Fixed
 			new OpLUT( "%dest = %cast(greaterThanEqual(%a,%b).%dm);\n",				0,  	true, 	true, true, 	null, null, true, null, true, null ),
 			new OpLUT( "%dest = %cast(lessThan(%a,%b).%dm);\n",						0,  	true, 	true, true, 	null, null, true, null, true, null ),
 			new OpLUT( "%dest = %cast(sign(%a));\n",								0,  	true, 	true, false, 	null, null, null, null, null, null ),
@@ -69,62 +66,3 @@ module aglsl
 			];
 	}
 }
-
-/*
-{ s:"%dest = %cast(%a);\n", flags:0, dest:true, a:true, b:false }, // mov
-{ s:"%dest = %cast(%a + %b);\n", flags:0, dest:true, a:true, b:true }, // add
-{ s:"%dest = %cast(%a - %b);\n", flags:0, dest:true, a:true, b:true }, // sub
-{ s:"%dest = %cast(%a * %b);\n", flags:0, dest:true, a:true, b:true }, // mul
-{ s:"%dest = %cast(%a / %b);\n", flags:0, dest:true, a:true, b:true }, // div
-{ s:"%dest = %cast(1.0) / %a;\n", flags:0, dest:true, a:true, b:false }, // rcp
-{ s:"%dest = %cast(min(%a,%b));\n", flags:0, dest:true, a:true, b:true }, // min
-{ s:"%dest = %cast(max(%a,%b));\n", flags:0, dest:true, a:true, b:true }, // max
-{ s:"%dest = %cast(fract(%a));\n", flags:0, dest:true, a:true, b:false }, // frc
-{ s:"%dest = %cast(sqrt(abs(%a)));\n", flags:0, dest:true, a:true, b:false }, // sqt
-{ s:"%dest = %cast(inversesqrt(abs(%a)));\n", flags:0, dest:true, a:true, b:false }, // rsq
-{ s:"%dest = %cast(pow(abs(%a),%b));\n", flags:0, dest:true, a:true, b:true }, // pow
-{ s:"%dest = %cast(log2(abs(%a)));\n", flags:0, dest:true, a:true, b:false }, // log
-{ s:"%dest = %cast(exp2(%a));\n", flags:0, dest:true, a:true, b:false }, // exp
-*/
-
-/*
-{ s:"%dest = %cast(normalize(%a));\n", flags:0, dest:true, a:true, b:false, ndwm:true }, // nrm
-{ s:"%dest = %cast(sin(%a));\n", flags:0, dest:true, a:true, b:false }, // sin
-{ s:"%dest = %cast(cos(%a));\n", flags:0, dest:true, a:true, b:false }, // cos
-{ s:"%dest = %cast(cross(vec3(%a),vec3(%b)));\n", flags:0, dest:true, a:true, b:true, ndwm:true }, // crs 
-{ s:"%dest = %cast(dot(vec3(%a),vec3(%b)));\n", flags:0, dest:true, a:true, b:true, ndwm:true }, // dp3
-{ s:"%dest = %cast(dot(vec4(%a),vec4(%b)));\n", flags:0, dest:true, a:true, b:true, ndwm:true }, // dp4
-{ s:"%dest = %cast(abs(%a));\n", flags:0, dest:true, a:true, b:false }, // abs
-{ s:"%dest = %cast(%a * -1.0);\n", flags:0, dest:true, a:true, b:false }, // neg
-{ s:"%dest = %cast(clamp(%a,0.0,1.0));\n", flags:0, dest:true, a:true, b:false }, // sat
-{ s:"%dest = %cast(dot(vec3(%a),vec3(%b)));\n", matrixwidth:3, matrixheight:3, dest:true, a:true, b:true, ndwm:true }, // m33 (uses dp3)
-{ s:"%dest = %cast(dot(vec4(%a),vec4(%b)));\n", matrixwidth:4, matrixheight:4, dest:true, a:true, b:true, ndwm:true }, // m44 (uses dp4)
-{ s:"%dest = %cast(dot(vec4(%a),vec4(%b)));\n", matrixwidth:4, matrixheight:3, dest:true, a:true, b:true, ndwm:true }, // m43 (uses dp4)
-*/
-
-/*
-{ s:"%dest = %cast(dFdx(%a));\n", flags:0, dest:true, a:true, b:false }, // dFdx
-{ s:"%dest = %cast(dFdy(%a));\n", flags:0, dest:true, a:true, b:false }, // dFdx  
-{ s:"if (float(%a)==float(%b)) {;\n", flags:0, dest:false, a:true, b:true, scalar:true }, 
-{ s:"if (float(%a)!=float(%b)) {;\n", flags:0, dest:false, a:true, b:true, scalar:true },
-{ s:"if (float(%a)>=float(%b)) {;\n", flags:0, dest:false, a:true, b:true, scalar:true },
-{ s:"if (float(%a)<float(%b)) {;\n", flags:0, dest:false, a:true, b:true, scalar:true },
-{ s:"} else {;\n", flags:0, dest:false, a:false, b:false }, 
-{ s:"};\n", flags:0, dest:false, a:false, b:false }, 
-{ },
-{ },
-{ },
-{ },
-*/
-
-/*
-//{ s:"%dest = %cast(texture%texdimLod(%b,%texsize(%a)).%dm);\n", dest:true, a:true, b:true, dm:true },
-//{ s:"if ( float(%a)<0.0 ) discard;\n", dest:false, a:true, b:false, scalar:true },
-//{ s:"%dest = %cast(texture%texdim(%b,%texsize(%a)%lod).%dm);\n", dest:true, a:true, b:true, lod:true, dm:true, ndwm:true },
-//{ s:"%dest = %cast(greaterThanEqual(%a,%b).%dm);\n", flags:0, dest:true, a:true, b:true, dm:true, ndwm:true }, 
-//{ s:"%dest = %cast(lessThan(%a,%b).%dm);\n", flags:0, dest:true, a:true, b:true, dm:true, ndwm:true }, 
-//{ s:"%dest = %cast(sign(%a));\n", flags:0, dest:true, a:true, b:false }, 
-//{ s:"%dest = %cast(equal(%a,%b).%dm);\n", flags:0, dest:true, a:true, b:true, dm:true, ndwm:true }, 
-//{ s:"%dest = %cast(notEqual(%a,%b).%dm);\n", flags:0, dest:true, a:true, b:true, dm:true, ndwm:true }
-// ....
-*/
