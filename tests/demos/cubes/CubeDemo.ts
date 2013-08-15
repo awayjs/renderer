d38abee6a1610683e3f2701b2b2e6e5a61b1d216
Karim Beyrouti <karim@kurst.co.uk>
2013-08-15 12:09:06 +0100

    CubeDemo - updated

--- tests/demos/cubes/CubeDemo.ts
+++ tests/demos/cubes/CubeDemo.ts
@@ -11,16 +11,10 @@ module demos.cubes
 	export class CubeDemo
 	{
 		
-		private _stage			: away.display.Stage;
-		
+
 		private _scene			: away.containers.Scene3D;
-		private _cam			: away.cameras.Camera3D;
-		private _renderer		: away.render.DefaultRenderer;
 		private _view			: away.containers.View3D;
-		
-		private _stageProxy		: away.managers.Stage3DProxy;
-		private _stageManager	: away.managers.Stage3DManager;
-		
+
 		private _cube      		: away.primitives.CubeGeometry;
 		private _torus       	: away.primitives.TorusGeometry;
 		private _mesh  			: away.entities.Mesh;
@@ -30,35 +24,21 @@ module demos.cubes
 			
 			away.Debug.THROW_ERRORS = false;
 			
-			this._stage = new away.display.Stage();
-			
-			this._cam = new away.cameras.Camera3D();
-			var p: away.cameras.PerspectiveLens = new away.cameras.PerspectiveLens( 40 );
-			
-			this._cam.lens = p;
-			
-			this._renderer = new away.render.DefaultRenderer();
-			this._scene = new away.containers.Scene3D();
-			
-			this._stageManager = away.managers.Stage3DManager.getInstance( this._stage );
-			this._stageProxy = this._stageManager.getStage3DProxy( 0 );
-			
-			this._renderer.iStage3DProxy = this._stageProxy;
-			
-			this._view = new away.containers.View3D( this._scene, this._cam, this._renderer );
-			this._view.stage3DProxy = this._stageProxy;
-			
-			this._cube = new away.primitives.CubeGeometry( 1.0, 1.0, 1.0 );
-			//this._torus = new away.primitives.TorusGeometry( 1.0, 0.5 );
-			this._mesh = new away.entities.Mesh( this._cube );
-			
-			this._scene.addChild( this._mesh );
-			
-			
-			
-			console.log( this._scene );
-			
-			this._view.render();
+			this._view              = new away.containers.View3D( );
+			this._cube              = new away.primitives.CubeGeometry( 100.0, 100.0, 100.0 );
+			this._mesh              = new away.entities.Mesh( this._cube );
+			this._view.scene.addChild( this._mesh );
+
+            document.onmousedown = ( e ) => this.onMouseDowm( e );
+
 		}
+
+        private onMouseDowm( e )
+        {
+
+            this._view.render();
+
+        }
+
 	}
 }
