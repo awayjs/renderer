c61beb3d2633b0ee14f21eeaedc1f3684120ece2
Karim Beyrouti <karim@kurst.co.uk>
2013-08-16 22:51:21 +0100

    CubeDemoTest - updated

--- tests/demos/cubes/CubeDemoTest.ts
+++ tests/demos/cubes/CubeDemoTest.ts
@@ -11,13 +11,11 @@ module demos.cubes
 	export class CubeDemoTest
 	{
 		
-
-		private _scene			: away.containers.Scene3D;
 		private _view			: away.containers.View3D;
-
-		private _cube      		: away.primitives.CubeGeometry;
-		private _torus       	: away.primitives.TorusGeometry;
+        private _cube      		: away.primitives.CubeGeometry;
+        private _torus          : away.primitives.TorusGeometry;
 		private _mesh  			: away.entities.Mesh;
+        private raf             : away.utils.RequestAnimationFrame;
 		
 		constructor()
 		{
@@ -26,17 +24,47 @@ module demos.cubes
 			
 			this._view              = new away.containers.View3D( );
 			this._cube              = new away.primitives.CubeGeometry( 100.0, 100.0, 100.0 );
-			this._mesh              = new away.entities.Mesh( this._cube );
+            this._torus             = new away.primitives.TorusGeometry(100, 50, 32, 16, false);
+			this._mesh              = new away.entities.Mesh( this._torus );
+
 			this._view.scene.addChild( this._mesh );
 
             document.onmousedown = ( e ) => this.onMouseDowm( e );
 
+            //this.raf = new away.utils.RequestAnimationFrame( this.render , this );
+            //this.raf.start();
+
+            //setInterval( ( ) => this.render , 100 );
+
+            //this.raf.start();
+
 		}
+
+        private render( dt : number = null )
+        {
+
+            try
+            {
+
+                this._mesh.rotationY -= 2;
+                //this._mesh.z -= 2;
+                this._view.render();
+
+            } catch(e)
+            {
+
+            }
+
+
+        }
 
         private onMouseDowm( e )
         {
 
-            this._view.render();
+            this.render();//.render();
+
+
+
 
         }
 
