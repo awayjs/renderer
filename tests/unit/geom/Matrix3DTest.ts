584b9f330ef1e1bd291a04c51cb06e6194936222
Gary Paluk <gary@plugin.io>
2013-08-19 21:11:56 +0100

    Added Matrix3D test placeholders

--- tests/unit/geom/Matrix3DTest.ts
+++ tests/unit/geom/Matrix3DTest.ts
@@ -31,6 +31,25 @@ module tests.unit.geom
 			this.areIdentical( m.rawData[13], 0 );
 			this.areIdentical( m.rawData[14], 0 );
 			this.areIdentical( m.rawData[15], 1 );
+			
+			var r:number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 ];
+			m = new away.geom.Matrix3D( r );
+			this.areIdentical( m.rawData[0], 0 );
+			this.areIdentical( m.rawData[1], 1 );
+			this.areIdentical( m.rawData[2], 2 );
+			this.areIdentical( m.rawData[3], 3 );
+			this.areIdentical( m.rawData[4], 4 );
+			this.areIdentical( m.rawData[5], 5 );
+			this.areIdentical( m.rawData[6], 6 );
+			this.areIdentical( m.rawData[7], 7 );
+			this.areIdentical( m.rawData[8], 8 );
+			this.areIdentical( m.rawData[9], 9 );
+			this.areIdentical( m.rawData[10], 10 );
+			this.areIdentical( m.rawData[11], 11 );
+			this.areIdentical( m.rawData[12], 12 );
+			this.areIdentical( m.rawData[13], 13 );
+			this.areIdentical( m.rawData[14], 14 );
+			this.areIdentical( m.rawData[15], 15 );
 		}
 		
 		public append():void
@@ -61,6 +80,11 @@ module tests.unit.geom
 			this.areIdentical( m1.rawData[15], 56 );
 		}
 		
+		public appendRotation():void
+		{
+			
+		}
+		
 		public appendScale():void
 		{
 			var m:away.geom.Matrix3D = new away.geom.Matrix3D();
@@ -81,6 +105,61 @@ module tests.unit.geom
 			this.areIdentical( m.rawData[13], 0 );
 			this.areIdentical( m.rawData[14], 0 );
 			this.areIdentical( m.rawData[15], 1 );
+		}
+		
+		public appendTranslation():void
+		{
+			
+		}
+		
+		public clone():void
+		{
+			
+		}
+		
+		public copyColumnFrom():void
+		{
+			
+		}
+		
+		public copyColumnTo():void
+		{
+			
+		}
+		
+		public copyFrom():void
+		{
+			
+		}
+		
+		public copyRawDataFrom():void
+		{
+			
+		}
+		
+		public copyRawDataTo():void
+		{
+			
+		}
+		
+		public copyRowFrom():void
+		{
+			
+		}
+		
+		public copyRowTo():void
+		{
+			
+		}
+		
+		public copyToMatrix3D():void
+		{
+			
+		}
+		
+		public decompose():void
+		{
+			
 		}
 		
 		public deltaTransformVector():void
@@ -117,6 +196,81 @@ module tests.unit.geom
 			this.areIdentical( m.rawData[13], 0 );
 			this.areIdentical( m.rawData[14], 0 );
 			this.areIdentical( m.rawData[15], 1 );
+		}
+		
+		public interpolate():void
+		{
+			// static
+		}
+		
+		public interpolateTo():void
+		{
+			
+		}
+		
+		public invert():void
+		{
+			
+		}
+		
+		public pointAt():void
+		{
+			
+		}
+		
+		public prepend():void
+		{
+			
+		}
+		
+		public prependRotation():void
+		{
+			
+		}
+		
+		public prependScale():void
+		{
+			
+		}
+		
+		public prependTranslation():void
+		{
+			
+		}
+		
+		public recompose():void
+		{
+			
+		}
+		
+		public transformVector():void
+		{
+			
+		}
+		
+		public transformVectors():void
+		{
+			
+		}
+		
+		public transpose():void
+		{
+			
+		}
+		
+		public getAxisRotation():void
+		{
+			
+		}
+		
+		public determinant():void
+		{
+			
+		}
+		
+		public position():void
+		{
+			
 		}
 	}
 }
