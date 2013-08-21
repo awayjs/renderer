6781b2d77d23f52e2c4359f919ca72dda163b6a8
Karim Beyrouti <karim@kurst.co.uk>
2013-08-21 12:34:21 +0100

    TorusLight - updated demo

--- tests/demos/lights/TorusLight.ts
+++ tests/demos/lights/TorusLight.ts
@@ -15,35 +15,24 @@ module demos.lights
 		private _scene			: away.containers.Scene3D;
 		private _view			: away.containers.View3D;
 
-		private _cube      		: away.primitives.CubeGeometry;
 		private _torus       	: away.primitives.TorusGeometry;
 		private _mesh  			: away.entities.Mesh;
-		private _mesh2  		: away.entities.Mesh;
-		
 		private _raf			:away.utils.RequestAnimationFrame;
 		private _image			:HTMLImageElement;
-		private _cameraAxis		:away.geom.Vector3D;
-		
 		private _light			:away.lights.DirectionalLight;
 		
 		constructor()
 		{
-			
-			away.Debug.THROW_ERRORS = false;
+
+            away.Debug.THROW_ERRORS = false;
+            away.Debug.ENABLE_LOG   = false;
+            away.Debug.LOG_PI_ERRORS = false;
+
 			
 			this._view              = new away.containers.View3D( );
-			
 			this._view.backgroundColor = 0x014C73;
-			this._view.camera.x = 130;
-			this._view.camera.y = 0;
-			this._view.camera.z = -300;
-			this._cameraAxis = new away.geom.Vector3D( 0, 0, 1 );
-			
-			this._view.camera.lens = new away.cameras.PerspectiveLens( 120 );
-			
-			this._cube              = new away.primitives.CubeGeometry( 20.0, 20.0, 20.0 );
-			this._torus             = new away.primitives.TorusGeometry( 150, 80, 32, 16, true );
-			
+			this._view.camera.lens = new away.cameras.PerspectiveLens( 60 );
+			this._torus             = new away.primitives.TorusGeometry( 120, 80, 32, 16, false );
 			
 			this.loadResources();
 		}
@@ -51,82 +40,57 @@ module demos.lights
 		private loadResources()
 		{
 			var urlRequest:away.net.URLRequest = new away.net.URLRequest( "dots.png" );
+
 			var imgLoader:away.net.IMGLoader = new away.net.IMGLoader();
-			imgLoader.addEventListener( away.events.Event.COMPLETE, this.imageCompleteHandler, this );
-			imgLoader.load( urlRequest );
+
+			    imgLoader.addEventListener( away.events.Event.COMPLETE, this.imageCompleteHandler, this );
+			    imgLoader.load( urlRequest );
 		}
 		
 		private imageCompleteHandler(e)
 		{
 			var imageLoader:away.net.IMGLoader = <away.net.IMGLoader> e.target
 			this._image = imageLoader.image;
-			
+
+            this._view.camera.z = -400;
 			var ts : away.textures.HTMLImageElementTexture = new away.textures.HTMLImageElementTexture( this._image, false );
 			
 			var light:away.lights.DirectionalLight = new away.lights.DirectionalLight();
-			light.color = 0xFFFFFF;
-			light.direction = new away.geom.Vector3D( 1, 0, 0 );
-			light.ambient = 0.6;
-			light.diffuse = 0.7;
-			light.specular = 1;
-			
+                light.color = 0xFFFFFF;
+                light.direction = new away.geom.Vector3D( 1, 0, 0 );
+                light.ambient = 0.6;
+                light.diffuse = 0.7;
+                light.specular = 1;
+
 			this._view.scene.addChild( light );
 			
 			var lightPicker:away.materials.StaticLightPicker = new away.materials.StaticLightPicker( [light] );
 			
 			var matTx: away.materials.TextureMaterial = new away.materials.TextureMaterial( ts, true, true, false );
-			matTx.lightPicker = lightPicker;
-			//matTx.blendMode = away.display.BlendMode.ADD;
-			//matTx.bothSides = true;
-			
+			    matTx.lightPicker = lightPicker;
+
 			this._mesh              = new away.entities.Mesh( this._torus, matTx );
-			this._mesh2              = new away.entities.Mesh( this._cube, matTx );
-			this._mesh2.x = 130;
-			this._mesh2.z = 40;
-			
 			this._view.scene.addChild( this._mesh );
-			this._view.scene.addChild( this._mesh2 );
-			
-			//this._raf = new away.utils.RequestAnimationFrame( this.render , this );
-            //this._raf.start();
+
+			this._raf = new away.utils.RequestAnimationFrame( this.render , this );
+            this._raf.start();
+
 			this.render( 0 );
 			
-            //this.resize( null );
 		}
 		
 		public render( dt:number = null ):void
 		{
-            this._view.camera.rotate( this._cameraAxis, 1 );
             this._mesh.rotationY += 1;
-            this._mesh2.rotationX += 0.4;
-            this._mesh2.rotationY += 0.4;
             this._view.render();
 		}
 		
-        public resize( e )
-        {
-            this._view.y         = 0;
-            this._view.x         = 0;
-            this._view.width     = window.innerWidth;
-            this._view.height    = window.innerHeight;
-            console.log( this._view.width , this._view.height );
-            this._view.render();
-        }
 
 	}
 }
 
-var test: demos.cubes.CubeDemo;
+var test: demos.lights.TorusLight;
 window.onload = function ()
 {
-    test = new demos.cubes.CubeDemo();
+    test = new demos.lights.TorusLight();
 }
-/*
-window.onresize = function ( e )
-{
-    if ( test )
-    {
-        test.resize( e );
-    }
-}
-*/
