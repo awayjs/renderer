a6881d9fb04205a0ceec78fc2346e611c1b773d6
Karim Beyrouti <karim@kurst.co.uk>
2013-08-28 21:44:23 +0100

    ObjectLibLoader - updated to work with changes in Away3D library ( yFlip for PerspectiveLens ). Cleaned up, and added test for dynamic resource management

--- tests/demos/parsers/ObjLibLoaderTest.ts
+++ tests/demos/parsers/ObjLibLoaderTest.ts
@@ -18,23 +18,24 @@ module demos.parsers
         private view    : away.containers.View3D;
         private raf     : away.utils.RequestAnimationFrame;
         private mesh    : away.entities.Mesh;
+        private meshes  : Array<away.entities.Mesh> = new Array<away.entities.Mesh>();
 
         constructor()
         {
 
-
             away.Debug.LOG_PI_ERRORS    = false;
             away.Debug.THROW_ERRORS     = false;
-
 
             this.view                  = new away.containers.View3D( );
             this.raf                    = new away.utils.RequestAnimationFrame( this.render , this );
 
-
-
-            //away.library.AssetLibrary.enableParser( loaders.JSONTextureParser) ;
             away.library.AssetLibrary.enableParser( away.loaders.OBJParser ) ;
 
+            /*
+            this.token = away.library.AssetLibrary.load(new away.net.URLRequest('atomArray.obj') );
+            this.token.addEventListener( away.events.LoaderEvent.RESOURCE_COMPLETE , this.onResourceComplete , this );
+            this.token.addEventListener(away.events.AssetEvent.ASSET_COMPLETE , this.onAssetComplete, this );
+             */
             this.token = away.library.AssetLibrary.load(new away.net.URLRequest('t800.obj') );
             this.token.addEventListener( away.events.LoaderEvent.RESOURCE_COMPLETE , this.onResourceComplete , this );
             this.token.addEventListener(away.events.AssetEvent.ASSET_COMPLETE , this.onAssetComplete, this );
@@ -44,69 +45,55 @@ module demos.parsers
         private render()
         {
 
-            console.log( 'render');
-            //*
-            if( this.mesh )
+            if( this.meshes )
             {
-                this.mesh.rotationY += 1;
+                for ( var c : number = 0 ; c < this.meshes.length ; c++ )
+                {
+                    this.meshes[c].rotationY += .14;
+                }
             }
 
             this.view.render();
-            //*/
+
         }
 
         public onAssetComplete ( e : away.events.AssetEvent )
         {
-
             console.log( '------------------------------------------------------------------------------');
             console.log( 'away.events.AssetEvent.ASSET_COMPLETE' , e.asset.name , away.library.AssetLibrary.getAsset(e.asset.name) );
             console.log( '------------------------------------------------------------------------------');
-
-            /*
-            this.mesh = <away.entities.Mesh> away.library.AssetLibrary.getAsset(e.asset.name);
-
-            this.view.scene.addChild( this.mesh );
-
-            //this.render();
-
-            document.onmousedown = () => this.render();
-              */
-            //this.raf.start();
-
-            /*
-            var htmlImageElementTexture : away.textures.HTMLImageElementTexture = <away.textures.HTMLImageElementTexture> away.library.AssetLibrary.getAsset(e.asset.name);
-
-            document.body.appendChild( htmlImageElementTexture.htmlImageElement );
-
-            htmlImageElementTexture.htmlImageElement.style.position = 'absolute';
-            htmlImageElementTexture.htmlImageElement.style.top = this.height + 'px';
-
-
-            this.height += ( htmlImageElementTexture.htmlImageElement.height + 10 ) ;
-            */
-
         }
         public onResourceComplete ( e : away.events.LoaderEvent )
         {
 
-            var loader : away.loaders.AssetLoader = <away.loaders.AssetLoader> e.target;
+            var loader  : away.loaders.AssetLoader   = <away.loaders.AssetLoader> e.target;
+            var l       : number                     = loader.baseDependency.assets.length//dependencies.length;
 
             console.log( '------------------------------------------------------------------------------');
             console.log( 'away.events.LoaderEvent.RESOURCE_COMPLETE' , e  );
             console.log( '------------------------------------------------------------------------------');
 
-            console.log( away.library.AssetLibrary.getAsset( 'Mesh_g0' ) ) ;
+            for ( var c : number = 0 ; c < l ; c ++ )
+            {
 
-            this.mesh = <away.entities.Mesh> away.library.AssetLibrary.getAsset( 'Mesh_g0' );
-            this.mesh.rotationX = 180;
-            this.mesh.y = 400;
-            this.mesh.scale( 4 );
+                var d : away.library.IAsset = loader.baseDependency.assets[c];
 
-            this.view.scene.addChild( this.mesh );
+                if ( d.assetType == away.library.AssetType.MESH )
+                {
+
+                    var mesh : away.entities.Mesh = <away.entities.Mesh> away.library.AssetLibrary.getAsset( d.name );
+                        mesh.y = -400;
+                        mesh.scale( 5 );
+
+                    this.view.scene.addChild( mesh );
+
+                    this.meshes.push( mesh );
+                }
+
+            }
 
             document.onmousedown = () => this.render();
             window.onresize = () => this.resize();
-
 
             this.raf.start();
 
