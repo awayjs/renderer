fe60d10ddcfb50835354f04d12d56998ac7d170d
Gary Paluk <gary@plugin.io>
2013-08-29 13:38:57 +0100

    Fixes to AGLSL mapping plus housekeeping

--- src/aglsl/Mapping.ts
+++ src/aglsl/Mapping.ts
@@ -9,17 +9,18 @@ module aglsl
 {
 	export class Mapping
 	{
-		static agal2glsllut:aglsl.OpLUT[] = [
+		static agal2glsllut:aglsl.OpLUT[] =
+		[
 			
 			//         s 												flags   dest    a     b 	    mw 	  mh    ndwm  scale dm	  lod
-			new OpLUT( "%dest = %cast(%a);\n", 							0, 		true,	true, false, 	null, null, null, null, null, null ), //mov     // 0
-			new OpLUT( "%dest = %cast(%a + %b);\n", 					0, 		true, 	true, true , 	null, null, null, null, null, null ), //add     // 1
-			new OpLUT( "%dest = %cast(%a - %b);\n", 					0, 		true, 	true, true , 	null, null, null, null, null, null ), //sub     // 2
-			new OpLUT( "%dest = %cast(%a * %b);\n", 					0, 		true, 	true, true , 	null, null, null, null, null, null ), //mul     // 3
-			new OpLUT( "%dest = %cast(%a / %b);\n", 					0, 		true, 	true, true , 	null, null, null, null, null, null ), //div     // 4
-			new OpLUT( "%dest = %cast(1.0) / %a;\n", 					0, 		true, 	true, false, 	null, null, null, null, null, null ), //rcp     // 5
-			new OpLUT( "%dest = %cast(min(%a,%b));\n", 					0, 		true, 	true, true , 	null, null, null, null, null, null ), //min     // 6
-			new OpLUT( "%dest = %cast(max(%a,%b));\n", 					0, 		true, 	true, true , 	null, null, null, null, null, null ), //max     // 7
+			new OpLUT( "%dest = %cast(%a);\n", 							0, 		true,	true, false, 	null, null, null, null, null, null ), //mov
+			new OpLUT( "%dest = %cast(%a + %b);\n", 					0, 		true, 	true, true , 	null, null, null, null, null, null ), //add
+			new OpLUT( "%dest = %cast(%a - %b);\n", 					0, 		true, 	true, true , 	null, null, null, null, null, null ), //sub
+			new OpLUT( "%dest = %cast(%a * %b);\n", 					0, 		true, 	true, true , 	null, null, null, null, null, null ), //mul
+			new OpLUT( "%dest = %cast(%a / %b);\n", 					0, 		true, 	true, true , 	null, null, null, null, null, null ), //div
+			new OpLUT( "%dest = %cast(1.0) / %a;\n", 					0, 		true, 	true, false, 	null, null, null, null, null, null ), //rcp
+			new OpLUT( "%dest = %cast(min(%a,%b));\n", 					0, 		true, 	true, true , 	null, null, null, null, null, null ), //min
+			new OpLUT( "%dest = %cast(max(%a,%b));\n", 					0, 		true, 	true, true , 	null, null, null, null, null, null ), //max
 			new OpLUT( "%dest = %cast(fract(%a));\n", 					0, 		true, 	true, false, 	null, null, null, null, null, null ), //frc
 			new OpLUT( "%dest = %cast(sqrt(abs(%a)));\n",				0, 		true, 	true, false, 	null, null, null, null, null, null ), //sqt
 			new OpLUT( "%dest = %cast(inversesqrt(abs(%a)));\n",		0, 		true, 	true, false, 	null, null, null, null, null, null ), //rsq
@@ -36,10 +37,10 @@ module aglsl
 			new OpLUT( "%dest = %cast(dot(vec4(%a),vec4(%b)));\n",		0, 	   	true, 	true, true, 	null, null, true, null, null, null ), //dp4
 			new OpLUT( "%dest = %cast(abs(%a));\n",						0, 	   	true, 	true, false, 	null, null, null, null, null, null ), //abs
 			new OpLUT( "%dest = %cast(%a * -1.0);\n",					0, 	   	true, 	true, false, 	null, null, null, null, null, null ), //neg
-            new OpLUT( "%dest = %cast(clamp(%a,0.0,1.0));\n",           0,      true,   true, false,    null, null, null, null, null, null ), //sat - fixed
-            new OpLUT( "%dest = %cast(dot(vec3(%a),vec3(%b)));\n",      null,   true,   true, true,     3,    3,    true, null, null, null ), //m33 - fixed
-            new OpLUT( "%dest = %cast(dot(vec4(%a),vec4(%b)));\n",      null,   true,   true, true,     4,    4,    true, null, null, null ), //m44 - fixed
-            new OpLUT( "%dest = %cast(dot(vec4(%a),vec4(%b)));\n",      null,   true,   true, true,     4,    3,    true, null, null, null ), //m43 - fixed
+            new OpLUT( "%dest = %cast(clamp(%a,0.0,1.0));\n",           0,      true,   true, false,    null, null, null, null, null, null ), //sat
+            new OpLUT( "%dest = %cast(dot(vec3(%a),vec3(%b)));\n",      null,   true,   true, true,     3,    3,    true, null, null, null ), //m33
+            new OpLUT( "%dest = %cast(dot(vec4(%a),vec4(%b)));\n",      null,   true,   true, true,     4,    4,    true, null, null, null ), //m44
+            new OpLUT( "%dest = %cast(dot(vec4(%a),vec4(%b)));\n",      null,   true,   true, true,     4,    3,    true, null, null, null ), //m43
 			new OpLUT( "%dest = %cast(dFdx(%a));\n",					0, 	   	true, 	true, false, 	null, null, null, null, null, null ), //dFdx
 			new OpLUT( "%dest = %cast(dFdx(%a));\n",					0, 	   	true, 	true, false, 	null, null, null, null, null, null ), //dFdx
 			new OpLUT( "if (float(%a)==float(%b)) {;\n",				0, 	   	false, 	true, true, 	null, null, null, true, null, null ),
@@ -56,13 +57,13 @@ module aglsl
 			//         s 															flags  	dest    a     b 	    mw 	  mh    ndwm  scale dm	  lod 
 			new OpLUT( "%dest = %cast(texture%texdimLod(%b,%texsize(%a)).%dm);\n",	null,  	true, 	true, true, 	null, null, null, null, true, null ),
 			new OpLUT( "if ( float(%a)<0.0 ) discard;\n",							null,  	false, 	true, false, 	null, null, null, true, null, null ),
-            new OpLUT( "%dest = %cast(texture%texdim(%b,%texsize(%a)%lod).%dm);\n", null,   true,   true, true,     null, null, true, null, true, true ), // Fixed
+            new OpLUT( "%dest = %cast(texture%texdim(%b,%texsize(%a)%lod).%dm);\n", null,   true,   true, true,     null, null, true, null, true, true ),
 			new OpLUT( "%dest = %cast(greaterThanEqual(%a,%b).%dm);\n",				0,  	true, 	true, true, 	null, null, true, null, true, null ),
 			new OpLUT( "%dest = %cast(lessThan(%a,%b).%dm);\n",						0,  	true, 	true, true, 	null, null, true, null, true, null ),
 			new OpLUT( "%dest = %cast(sign(%a));\n",								0,  	true, 	true, false, 	null, null, null, null, null, null ),
-			new OpLUT( "%dest = %cast(equal(%a,%b).%dm);\n",						0,  	true, 	true, false, 	null, null, true, null, true, null ),
-			new OpLUT( "%dest = %cast(notEqual(%a,%b).%dm);\n",						0,  	true, 	true, true, 	null, null, true, null, true, null ),
+			new OpLUT( "%dest = %cast(equal(%a,%b).%dm);\n",						0,  	true, 	true, true, 	null, null, true, null, true, null ),
+			new OpLUT( "%dest = %cast(notEqual(%a,%b).%dm);\n",						0,  	true, 	true, true, 	null, null, true, null, true, null )
 			
-			];
+		];
 	}
 }
--- src/aglsl/assembler/AGALMiniAssembler.ts
+++ src/aglsl/assembler/AGALMiniAssembler.ts
@@ -63,7 +63,8 @@ module aglsl.assembler
 			
 			// get opcode/command				            
 			var tokens = line.match( /([\w\.\+\[\]]+)/gi ); // get tokens in line
-			if ( !tokens || tokens.length<1 ) {
+			if ( !tokens || tokens.length<1 )
+			{
 				if ( line.length >= 3 )
 				{
 					console.log ( "Warning: bad line "+linenr+": "+line );
@@ -72,25 +73,30 @@ module aglsl.assembler
 			}
 			
 			//console.log ( linenr, line, cur, tokens ); 
-			switch ( tokens[0] ) {
+			switch ( tokens[0] )
+			{
 				case "part":
 					this.addHeader( tokens[1], Number(tokens[2]) ); 
 					break;                
 				case "endpart":
-					if ( !this.cur ) throw "Unexpected endpart";  
+					if ( !this.cur )
+					{
+						throw "Unexpected endpart";
+					}
 					this.cur.data.position = 0;
 					this.cur = null; 
 					return; 
 				default: 
-					if ( !this.cur ) {
-						console.log ( "Warning: bad line "+linenr+": "+line+" (Outside of any part definition)" );
+					if( !this.cur )
+					{
+						console.log( "Warning: bad line "+linenr+": "+line+" (Outside of any part definition)" );
 						return; 
 					}
-					if ( this.cur.name=="comment" )
+					if ( this.cur.name == "comment" )
 					{
 						return;
 					}
-					var op : aglsl.assembler.Opcode = <aglsl.assembler.Opcode> OpcodeMap.map[tokens[0]];
+					var op: aglsl.assembler.Opcode = <aglsl.assembler.Opcode> OpcodeMap.map[tokens[0]];
 					if ( !op )
 					{
 						throw "Bad opcode "+tokens[0]+" "+linenr+": "+line;
@@ -105,38 +111,57 @@ module aglsl.assembler
 						{
 							throw "Bad destination register "+tokens[ti-1]+" "+linenr+": "+line;
 						}
-					} else this.emitZeroDword ( this.cur ); 
-					if ( op.a && op.a.format!="none" ) {
-						if ( !this.emitSource ( this.cur, tokens[ti++], op.a ) ) throw "Bad source register "+tokens[ti-1]+" "+linenr+": "+line; 
-					} else this.emitZeroQword ( this.cur ); 
-					if ( op.b && op.b.format!="none" ) {
-						if ( op.b.format == "sampler" ) {
-							if ( !this.emitSampler ( this.cur, tokens[ti++], op.b, opts ) )
+					}
+					else 
+					{
+						this.emitZeroDword( this.cur );
+					}
+					
+					if ( op.a && op.a.format!="none" )
+					{
+						if ( !this.emitSource( this.cur, tokens[ti++], op.a ) ) throw "Bad source register "+tokens[ti-1]+" "+linenr+": "+line; 
+					}
+					else
+					{
+						this.emitZeroQword( this.cur ); 
+					}
+					
+					if ( op.b && op.b.format!="none" )
+					{
+						if ( op.b.format == "sampler" )
+						{
+							if( !this.emitSampler( this.cur, tokens[ti++], op.b, opts ) )
 							{
 								throw "Bad sampler register "+tokens[ti-1]+" "+linenr+": "+line;
 							}
-						} else {
-							if ( !this.emitSource ( this.cur, tokens[ti++], op.b ) )
+						}
+						else
+						{
+							if ( !this.emitSource( this.cur, tokens[ti++], op.b ) )
 							{
-								throw "Bad source register "+tokens[ti-1]+" "+linenr+": "+line;
+								throw "Bad source register " + tokens[ti-1] + " " + linenr + ": " + line;
 							}
 						}
-					} else this.emitZeroQword ( this.cur ); 
-					
-					break; 
+					}
+					else
+					{
+						this.emitZeroQword( this.cur ); 
+					}
+					break;
 			}                			                                             
 		}
 		
 		public emitHeader( pr:aglsl.assembler.Part )
 		{
-			pr.data.writeUnsignedByte ( 0xa0 ); 	// tag version
-			pr.data.writeUnsignedInt ( pr.version ); 
-			if ( pr.version >= 0x10 )
+			pr.data.writeUnsignedByte( 0xa0 ); 	// tag version
+			pr.data.writeUnsignedInt( pr.version ); 
+			if( pr.version >= 0x10 )
 			{
 				pr.data.writeUnsignedByte ( 0 ); // align, for higher versions
 			}
 			pr.data.writeUnsignedByte ( 0xa1 );		// tag program id
-			switch( pr.name ) {
+			switch( pr.name )
+			{
 				case "fragment" : pr.data.writeUnsignedByte ( 1 ); break;
 				case "vertex" : pr.data.writeUnsignedByte ( 0 ); break;
 				case "cpu" : pr.data.writeUnsignedByte ( 2 ); break;
@@ -144,21 +169,25 @@ module aglsl.assembler
 			}                        
 		}    
 		
-		public emitOpcode ( pr:aglsl.assembler.Part, opcode ) {
+		public emitOpcode( pr:aglsl.assembler.Part, opcode )
+		{
 			pr.data.writeUnsignedInt( opcode );
 			//console.log ( "Emit opcode: ", opcode ); 
 		}
 		
-		public emitZeroDword( pr:aglsl.assembler.Part ) {
+		public emitZeroDword( pr:aglsl.assembler.Part )
+		{
 			pr.data.writeUnsignedInt( 0 );
 		}
 		
-		public emitZeroQword ( pr ) {
+		public emitZeroQword( pr )
+		{
 			pr.data.writeUnsignedInt( 0 );
 			pr.data.writeUnsignedInt( 0 );
 		}
 		
-		public emitDest( pr, token, opdest ) {
+		public emitDest( pr, token, opdest )
+		{
 			
             //console.log( 'aglsl.assembler.AGALMiniAssembler' , 'emitDest' , 'RegMap.map' , RegMap.map);           
 			var reg = token.match ( /([fov]?[tpocidavs])(\d*)(\.[xyzw]{1,4})?/i ); // g1: regname, g2:regnum, g3:mask
@@ -186,24 +215,62 @@ module aglsl.assembler
 			return r;
 		} 
 		
-		public stringToSwizzle( s ) {
-			if ( !s ) return 0xe4; 
+		public stringToSwizzle( s )
+		{
+			if( !s )
+			{
+				return 0xe4;
+			}
 			var chartoindex = { x:0, y:1, z:2, w:3 };
 			var sw = 0;
-			if ( s.charAt(0) != "." ) throw "Missing . for swizzle";
-			if ( s.length > 1) sw |= chartoindex[s.charAt(1)];             
-			if ( s.length > 2 ) sw |= chartoindex[s.charAt(2)]<<2; 
-			else sw |= (sw&3)<<2;
-			if ( s.length > 3) sw |= chartoindex[s.charAt(3)]<<4; 
-			else sw |= (sw&(3<<2))<<2;
-			if ( s.length > 4) sw |= chartoindex[s.charAt(4)]<<6; 
-			else sw |= (sw&(3<<4))<<2;
+			
+			if( s.charAt(0) != "." )
+			{
+				throw "Missing . for swizzle";
+			}
+			
+			if( s.length > 1)
+			{
+				sw |= chartoindex[s.charAt(1)];
+			}
+			
+			if( s.length > 2 )
+			{
+				sw |= chartoindex[s.charAt(2)]<<2;
+			}
+			else
+			{
+				sw |= (sw&3)<<2;
+			}
+			
+			if( s.length > 3)
+			{
+				sw |= chartoindex[s.charAt(3)]<<4;
+			}
+			else
+			{
+				sw |= (sw&(3<<2))<<2;
+			}
+			
+			if( s.length > 4)
+			{
+				sw |= chartoindex[s.charAt(4)]<<6;
+			}
+			else
+			{
+				sw |= (sw&(3<<4))<<2;
+			}
+			
 			return sw; 
 		}
 		
-		public emitSampler( pr: aglsl.assembler.Part, token, opsrc, opts ) {                            
-			var reg = token.match ( /fs(\d*)/i ); // g1:regnum
-			if ( !reg || !reg[1] ) return false; 
+		public emitSampler( pr: aglsl.assembler.Part, token, opsrc, opts )
+		{
+			var reg = token.match( /fs(\d*)/i ); // g1:regnum
+			if( !reg || !reg[1] )
+			{
+				return false;
+			}
 			pr.data.writeUnsignedShort ( reg[1] ); 
 			pr.data.writeUnsignedByte ( 0 );   // bias
 			pr.data.writeUnsignedByte ( 0 );         
@@ -213,21 +280,27 @@ module aglsl.assembler
 			pr.data.writeUnsignedByte ( 0 );   // special, wrap        
 			pr.data.writeUnsignedByte ( 0 );   // mip, filter                            
 			*/
-			var samplerbits = 0x5; 
-			var sampleroptset = 0; 
-			for ( var i=0; i<opts.length; i++ ) {
-				var o : aglsl.assembler.Sampler = <aglsl.assembler.Sampler> SamplerMap.map[opts[i].toLowerCase()];
+			var samplerbits:number = 0x5; 
+			var sampleroptset:number = 0; 
+			for ( var i:number = 0; i < opts.length; i++ )
+			{
+				var o:aglsl.assembler.Sampler = <aglsl.assembler.Sampler> SamplerMap.map[opts[i].toLowerCase()];
 				
                 //console.log( 'AGALMiniAssembler' , 'emitSampler' , 'SampleMap opt:' , o , '<-------- WATCH FOR THIS');
 				
 				if( o )
 				{
-					if ( ((sampleroptset>>o.shift)&o.mask)!=0 ) console.log ("Warning, duplicate sampler option");
-					sampleroptset |= o.mask<<o.shift;
-					samplerbits &= ~(o.mask<<o.shift);
-					samplerbits |= o.value<<o.shift;
-				} else {
-					console.log ("Warning, unknown sampler option: ", opts[i] );
+					if ( ((sampleroptset >> o.shift) & o.mask) != 0 )
+					{
+						console.log("Warning, duplicate sampler option");
+					}
+					sampleroptset |= o.mask << o.shift;
+					samplerbits &= ~(o.mask << o.shift);
+					samplerbits |= o.value << o.shift;
+				}
+				else
+				{
+					console.log("Warning, unknown sampler option: ", opts[i] );
 					// todo bias
 				}
 			}
@@ -235,37 +308,48 @@ module aglsl.assembler
 			return true;
 		}
 		
-		public emitSource( pr, token, opsrc ) {
-			var indexed = token.match ( /vc\[(v[tcai])(\d+)\.([xyzw])([\+\-]\d+)?\](\.[xyzw]{1,4})?/i ); // g1: indexregname, g2:indexregnum, g3:select, [g4:offset], [g5:swizzle] 
+		public emitSource( pr, token, opsrc )
+		{
+			var indexed = token.match( /vc\[(v[tcai])(\d+)\.([xyzw])([\+\-]\d+)?\](\.[xyzw]{1,4})?/i ); // g1: indexregname, g2:indexregnum, g3:select, [g4:offset], [g5:swizzle] 
 			var reg;
-			if ( indexed ) {
-				if ( !RegMap.map[indexed[1]] ) return false;          
+			if( indexed )
+			{
+				if ( !RegMap.map[indexed[1]] )
+				{
+					return false;
+				}
 				var selindex = { x:0, y:1, z:2, w:3 };
-				var em : any = { num:indexed[2]|0, code:RegMap.map[indexed[1]].code, swizzle:this.stringToSwizzle(indexed[5]), select:selindex[indexed[3]], offset:indexed[4]|0 };
-				pr.data.writeUnsignedShort ( em.num );      
-				pr.data.writeByte ( em.offset );                     
-				pr.data.writeUnsignedByte ( em.swizzle ); 
-				pr.data.writeUnsignedByte ( 0x1 ); // constant reg
-				pr.data.writeUnsignedByte ( em.code ); 
-				pr.data.writeUnsignedByte ( em.select ); 
-				pr.data.writeUnsignedByte ( 1<<7 );                         				                                
-			} else {
-				reg = token.match ( /([fov]?[tpocidavs])(\d*)(\.[xyzw]{1,4})?/i ); // g1: regname, g2:regnum, g3:swizzle
-				if ( !RegMap.map[reg[1]] ) return false;          
+				var em:any = { num:indexed[2]|0, code:RegMap.map[indexed[1]].code, swizzle:this.stringToSwizzle(indexed[5]), select:selindex[indexed[3]], offset:indexed[4]|0 };
+				pr.data.writeUnsignedShort( em.num );
+				pr.data.writeByte( em.offset );
+				pr.data.writeUnsignedByte( em.swizzle );
+				pr.data.writeUnsignedByte( 0x1 ); // constant reg
+				pr.data.writeUnsignedByte( em.code );
+				pr.data.writeUnsignedByte( em.select );
+				pr.data.writeUnsignedByte( 1 << 7 );
+			}
+			else
+			{
+				reg = token.match( /([fov]?[tpocidavs])(\d*)(\.[xyzw]{1,4})?/i ); // g1: regname, g2:regnum, g3:swizzle
+				if( !RegMap.map[reg[1]] )
+				{
+					return false;
+				}
 				var em : any = { num:reg[2]|0, code:RegMap.map[reg[1]].code, swizzle:this.stringToSwizzle(reg[3]) };
-				pr.data.writeUnsignedShort ( em.num );      
-				pr.data.writeUnsignedByte ( 0 ); 
-				pr.data.writeUnsignedByte ( em.swizzle ); 
-				pr.data.writeUnsignedByte ( em.code ); 
-				pr.data.writeUnsignedByte ( 0 ); 
-				pr.data.writeUnsignedByte ( 0 ); 
-				pr.data.writeUnsignedByte ( 0 );                             
-				//console.log ( "  Emit source: ", em, pr.data.length );             
+				pr.data.writeUnsignedShort ( em.num );
+				pr.data.writeUnsignedByte ( 0 );
+				pr.data.writeUnsignedByte ( em.swizzle );
+				pr.data.writeUnsignedByte ( em.code );
+				pr.data.writeUnsignedByte ( 0 );
+				pr.data.writeUnsignedByte ( 0 );
+				pr.data.writeUnsignedByte ( 0 );
+				//console.log ( "  Emit source: ", em, pr.data.length );
 			}                    
 			return true; 
 		}  
 		
-		public addHeader( partname, version ) {
+		public addHeader( partname, version )
+		{
 			if( !version )
 			{
 				version = 1;
@@ -281,6 +365,5 @@ module aglsl.assembler
 			}
 			this.cur = this.r[partname]; 
 		}
-		
 	}
 }
