6cfb510e8468e40e05b365c0239ee78f90609075
Karim Beyrouti <karim@kurst.co.uk>
2013-09-05 15:36:22 +0100

    definitions - addeed HTMLImageElementCubeTexture

--- src/away/_definitions.ts
+++ src/away/_definitions.ts
@@ -158,6 +158,9 @@
 ///<reference path="textures/BitmapTexture.ts" />
 ///<reference path="textures/CubeTextureBase.ts" />
 ///<reference path="textures/RenderTexture.ts" />
+
+///<reference path="textures/HTMLImageElementCubeTexture.ts" />
+
 ///<reference path="utils/TextureUtils.ts" />
 ///<reference path="events/TimerEvent.ts" />
 ///<reference path="events/ParserEvent.ts" />
