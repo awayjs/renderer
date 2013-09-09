ba4754e2150b839b4af304b92e3fdf5b28481119
Karim Beyrouti <karim@kurst.co.uk>
2013-09-09 16:15:52 +0100

    AWDSuzane - added ( and commented out ) hover controller....

--- tests/demos/parsers/AWDSuzanne.ts
+++ tests/demos/parsers/AWDSuzanne.ts
@@ -18,6 +18,13 @@ module demos.parsers {
         private _suzane         : away.entities.Mesh;
         private _light          : away.lights.DirectionalLight;
         private _lightPicker    : away.materials.StaticLightPicker;
+        private _hoverControl   : away.controllers.HoverController;
+
+        private _move           : boolean = false;
+        private _lastPanAngle   : number;
+        private _lastTiltAngle  : number;
+        private _lastMouseX     : number;
+        private _lastMouseY     : number;
 
         constructor()
         {
@@ -43,6 +50,8 @@ module demos.parsers {
             this._light.specular         = 1.8;
             this._view.scene.addChild( this._light );
 
+
+
             this._lightPicker           = new away.materials.StaticLightPicker( [this._light] );
 
             window.onresize = () => this.resize();
@@ -60,15 +69,21 @@ module demos.parsers {
         private render( dt : number ) //animate based on dt for firefox
         {
 
-            /*
+
             if ( this._suzane )
             {
                 this._suzane.rotationY += 1;
             }
-            */
 
             this._view.render();
-            this._view.camera.z = -2000;
+
+            if ( this._hoverControl  )
+            {
+
+                this._hoverControl.update(false);
+
+            }
+
         }
 
         public onAssetComplete ( e : away.events.AssetEvent )
@@ -99,13 +114,15 @@ module demos.parsers {
                     case away.library.AssetType.MESH:
 
                         var mesh : away.entities.Mesh = <away.entities.Mesh> asset;
-                            mesh.scale( 600 );
+                            mesh.scale( 400 );
 
                         this._suzane = mesh;
                         this._suzane.material.lightPicker = this._lightPicker;
+                        this._suzane.y = -100;
 
                         this._view.scene.addChild( mesh );
                         this._timer.start();
+
 
                         this.resize();
 
@@ -128,9 +145,52 @@ module demos.parsers {
 
             }
 
+            /*
+            this._hoverControl = new away.controllers.HoverController( this._view.camera , this._suzane );
+            document.onmousedown = ( e ) => this.onMouseDownHandler( e );
+            document.onmouseup = ( e ) => this.onMouseUpHandler( e );
+            document.onmousemove = ( e ) => this.onMouseMove( e );
+            */
 
         }
 
+        /*
+
+        private onMouseUpHandler( e  )
+        {
+            this._move = false;
+        }
+
+        private onMouseMove( e )
+        {
+
+
+            if (this._move)
+            {
+
+                this._hoverControl.panAngle = 0.3 * (window.event.clientX - this._lastMouseX) + this._lastPanAngle;
+                this._hoverControl.tiltAngle = 0.3 * (window.event.clientY - this._lastMouseY) + this._lastTiltAngle;
+            }
+
+
+
+        }
+
+        private onMouseDownHandler( e  )
+        {
+
+            this._lastPanAngle      = this._hoverControl.panAngle;
+            this._lastTiltAngle     = this._hoverControl.tiltAngle;
+
+            this._lastMouseX        = window.event.clientX;//e.clientX;
+            this._lastMouseY        = window.event.clientY;//e.clientX;
+
+            this._move              = true;
+
+
+        }
+
+        */
     }
 
 }
