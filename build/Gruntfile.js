8b8275976c6ebbabb8aba62904c094b6cc3130fb
Karim Beyrouti <karim@kurst.co.uk>
2013-09-13 10:05:59 +0100

    Gruntfile.js - removed all other modules to test out error in grunt-typescript module

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -1,21 +1,3 @@
-/********************************************************************************
- *
- *   usage:
- *
- ********************************************************************************
- *
- *       Default Tasks:      grunt --libversion=0.0.1
- *       TypeScript Only:    grunt ts
- *       Documentation Only: grunt doc
- *       Minify Only:        grunt min
- *
- ********************************************************************************
- *
- *  libversion = verion number of the lib defaults to 'next' if no
- *  version is specified
- *
- ********************************************************************************/
-
 module.exports = function(grunt) {
 
     var version = grunt.option('libversion') || 'next';
@@ -23,9 +5,6 @@ module.exports = function(grunt) {
     // Plugins used by this Grunt Script
 
     grunt.loadNpmTasks('grunt-typescript');
-    //grunt.loadNpmTasks('grunt-contrib-yuidoc');
-    //grunt.loadNpmTasks('grunt-contrib-uglify');
-    //grunt.loadNpmTasks('grunt-contrib-concat');
 
 
     // Configuration
@@ -35,22 +14,6 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json'),
 
         // Metadata / Configuration
-
-        meta: {
-
-            v:grunt.option('vers') || "",
-
-            basePath: '../',
-            tsFile: '../src/Away3D.ts',
-            tsPath: '../src/',
-            tsExportFile: '../lib/Away3D.' + version + '.js',
-            tsExportUglyFile: '../lib/Away3D.' + version + '.min.js',
-            tsExportFolder: '../lib/',
-            tsMultiExportFolder: '../lib/src/',
-            docsPath: '../docs/',
-            deployPath: '../lib/'
-
-        },
 
         // Export TypeScript Source
 
@@ -68,74 +31,6 @@ module.exports = function(grunt) {
                 }
             }
 
-            /*
-            MainJsFile: { // Export Concatenated JavaScript from Main TS file
-                src: ['<%= meta.tsFile %>'],
-                dest: '<%= meta.tsExportFile %>',
-                options: {
-                }
-            },
-
-            MultipleJSFiles: { // Export multiple JavaScript files from TS source
-
-                src: ['<%= meta.tsFile %>'],
-                dest: '<%= meta.tsMultiExportFolder %>',
-                options: {
-                }
-            }
-             */
-        },
-
-        concat: {
-
-            options: {
-                // define a string to put between each file in the concatenated output
-                separator: ';'
-            },
-            dist: {
-                // the files to concatenate
-                src: [ '../src/ts/libs/preloadjs-0.3.1.min.js' , '<%= meta.tsExportFile %>' ],
-                // the location of the resulting JS file
-                dest: '../deploy/TEST.js'
-
-            }
-        },
-
-        // Minify
-
-        uglify: {
-
-            my_target: {
-
-                files: {
-
-                    '<%= meta.tsExportUglyFile %>': [ '<%= meta.tsExportFile %>' ]
-                }
-
-            }
-
-        },
-
-        // Export Documentation ( using multi export JS files )
-
-        yuidoc: {
-
-            compile: {
-
-                name: '<%= pkg.name %>',
-                description: '<%= pkg.description %>',
-                version: '<%= pkg.version %>',
-                url: '<%= pkg.homepage %>',
-
-                options: {
-
-                    paths: '<%= meta.tsMultiExportFolder %>',
-                    outdir: '<%= meta.docsPath %>'
-
-                }
-
-            }
-
         }
 
     } );
@@ -143,10 +38,6 @@ module.exports = function(grunt) {
     grunt.option.init();
 
     grunt.registerTask('default', ['typescript' ]); // Default Tasks
-    //grunt.registerTask('default', ['typescript' , 'concat', 'uglify' , 'yuidoc'  ]); // Default Tasks
-    //grunt.registerTask('ts', [ 'typescript' ]); // Export TypeScript only
-    //grunt.registerTask('doc', [ 'yuidoc' ]); // Export Documentation only
-    //grunt.registerTask('min', [ 'uglify' ]); // Minify only
 
 };
 
