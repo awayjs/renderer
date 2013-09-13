c67c40c8e8d610510813b714114e3764f983cfbe
Karim Beyrouti <karim@kurst.co.uk>
2013-09-13 16:31:47 +0100

    docs - WIP

---
+++ docs/api.js
@@ -0,0 +1,36 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "away.base.Geometry",
+        "away.base.Object3D",
+        "away.base.SkinnedSubGeometry",
+        "away.base.SubGeometry",
+        "away.base.SubGeometryBase",
+        "away.events.AssetEvent",
+        "away.events.CameraEvent",
+        "away.events.Event",
+        "away.events.EventDispatcher",
+        "away.events.GeometryEvent",
+        "away.events.HTTPStatusEvent"
+    ],
+    "modules": [
+        "away.base",
+        "away.events",
+        "away.geom"
+    ],
+    "allModules": [
+        {
+            "displayName": "away.base",
+            "name": "away.base"
+        },
+        {
+            "displayName": "away.events",
+            "name": "away.events"
+        },
+        {
+            "displayName": "away.geom",
+            "name": "away.geom"
+        }
+    ]
+} };
+});
