a6046c2e926f904899df137a8abb14e07367ce86
Karim Beyrouti <karim@kurst.co.uk>
2013-09-13 16:20:30 +0100

    Disable PartialImplementationErrors and Logs for release builds - this will be removed in future versions

--- src/Away3D.ts
+++ src/Away3D.ts
@@ -5,6 +5,9 @@
 
 ///<reference path="away/_definitions.ts"/>
 
+away.Debug.THROW_ERRORS     = false;
+away.Debug.LOG_PI_ERRORS    = false;
+
 module away
 {
 
@@ -20,4 +23,5 @@ module away
     }
 
 }
+
 
