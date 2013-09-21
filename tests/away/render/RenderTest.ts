387753d2280ba4088958c8dec75628305c0e7e10
Karim Beyrouti <karim@kurst.co.uk>
2013-09-21 19:27:05 +0100

    Updated test to work with compiled Away3D JavaScript / TypeScript definitions file and new app harness. Removed JS / HTML

--- tests/away/render/RenderTest.ts
+++ tests/away/render/RenderTest.ts
@@ -1,62 +1,58 @@
-///<reference path="../../../src/away/_definitions.ts" />
+///<reference path="../../../lib/Away3D.next.d.ts" />
+//<reference path="../../../src/Away3D.ts" />
 
-//------------------------------------------------------------------------------------------------
-// Web / PHP Storm arguments string
-//------------------------------------------------------------------------------------------------
-// --sourcemap $ProjectFileDir$/tests/away/managers/ManagersTest.ts --target ES5 --comments --out $ProjectFileDir$/tests/away/managers/ManagersTest.js
-//------------------------------------------------------------------------------------------------
-
-class RenderTest
+module tests.render
 {
-
-    private stage           : away.display.Stage;
-    private sManager        : away.managers.Stage3DManager;
-    private sProxy          : away.managers.Stage3DProxy;
-
-    private dRenderer       : away.render.DefaultRenderer;
-    constructor()
+    export class RenderTest
     {
 
-        away.Debug.THROW_ERRORS  = false;
+        private stage           : away.display.Stage;
+        private sManager        : away.managers.Stage3DManager;
+        private sProxy          : away.managers.Stage3DProxy;
 
-        this.stage      = new away.display.Stage();
-        this.sManager   = away.managers.Stage3DManager.getInstance( this.stage );
-        this.sProxy     = this.sManager.getStage3DProxy( 0 );
+        private dRenderer       : away.render.DefaultRenderer;
+        constructor()
+        {
 
-        this.dRenderer = new away.render.DefaultRenderer();
-        console.log( this.dRenderer );
+            away.Debug.THROW_ERRORS  = false;
 
+            this.stage      = new away.display.Stage();
+            this.sManager   = away.managers.Stage3DManager.getInstance( this.stage );
+            this.sProxy     = this.sManager.getStage3DProxy( 0 );
+
+            console.log( 'away.display.Stage' , this.stage );
+            console.log( 'away.managers.Stage3DManager' , this.sManager );
+            console.log( 'away.managers.Stage3DProxy' , this.sProxy );
+
+
+            this.dRenderer = new away.render.DefaultRenderer();
+
+            console.log( 'away.render.DefaultRenderer' , this.dRenderer );
+
+
+
+        }
+
+        public onContextCreated( e : away.events.Stage3DEvent ) : void
+        {
+
+            away.Debug.log( 'onContextCreated' , e );
+
+        }
+
+        public onContextReCreated( e : away.events.Stage3DEvent ) : void
+        {
+
+            away.Debug.log( 'onContextReCreated' , e );
+
+        }
+
+        public onContextDisposed( e : away.events.Stage3DEvent ) : void
+        {
+
+            away.Debug.log( 'onContextDisposed' , e );
+
+        }
 
     }
-
-    public onContextCreated( e : away.events.Stage3DEvent ) : void
-    {
-
-        away.Debug.log( 'onContextCreated' , e );
-
-    }
-
-    public onContextReCreated( e : away.events.Stage3DEvent ) : void
-    {
-
-        away.Debug.log( 'onContextReCreated' , e );
-
-    }
-
-    public onContextDisposed( e : away.events.Stage3DEvent ) : void
-    {
-
-        away.Debug.log( 'onContextDisposed' , e );
-
-    }
-
-}
-
-var test: RenderTest;
-window.onload = function ()
-{
-
-
-    test = new RenderTest();
-
 }
