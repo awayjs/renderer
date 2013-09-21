ab8235a7db76a31514d3f3fcf4798a5ece9c5e45
Karim Beyrouti <karim@kurst.co.uk>
2013-09-21 19:59:40 +0100

    Updated test to work with compiled Away3D JavaScript / TypeScript definitions file and new app harness. Removed JS / HTML

--- tests/away/primitives/PrimitivesTest.ts
+++ tests/away/primitives/PrimitivesTest.ts
@@ -1,39 +1,120 @@
-///<reference path="../../../src/away/_definitions.ts" />
+///<reference path="../../../lib/Away3D.next.d.ts" />
+//<reference path="../../../src/Away3D.ts" />
 
+module tests.primitives
+{
 
-//------------------------------------------------------------------------------------------------
-// Web / PHP Storm arguments string
-//------------------------------------------------------------------------------------------------
-// --sourcemap $ProjectFileDir$/tests/away/pick/PickingTests.ts --target ES5 --comments --out $ProjectFileDir$/tests/away/pick/PickingTests.js
-//------------------------------------------------------------------------------------------------
-
-
-module tests {
-
-    export class PrimitivesTest //extends away.events.EventDispatcher
+    export class PrimitivesTest//extends away.events.EventDispatcher
     {
 
-        private torus : away.primitives.TorusGeometry;
+        private view                : away.containers.View3D;
+        private raf                 : away.utils.RequestAnimationFrame;
+        private meshes              : Array<away.entities.Mesh> = new Array<away.entities.Mesh>();
+        private light               : away.lights.DirectionalLight;
+        private lightB              : away.lights.DirectionalLight;
+        private staticLightPicker   : away.materials.StaticLightPicker;
+        //private t                   : number = 0;
+        private radius              : number = 400;
+
 
         constructor()
         {
 
-            this.torus = new away.primitives.TorusGeometry();
-            this.torus.iValidate();
-            console.log( this.torus );
+            away.Debug.LOG_PI_ERRORS    = false;
+            away.Debug.THROW_ERRORS     = false;
+
+            this.view                   = new away.containers.View3D( );
+            this.raf                    = new away.utils.RequestAnimationFrame( this.render , this );
+
+            this.light                  = new away.lights.DirectionalLight();
+            this.light.color            = 0xFFFFFF;
+            this.light.direction        = new away.geom.Vector3D( 1 , 1 ,0 );
+            this.light.ambient          = 0;//0.05;//.4;
+            this.light.ambientColor     = 0xFFFFFF;
+            this.light.diffuse          = 1;
+            this.light.specular         = 1;
+
+            this.lightB                  = new away.lights.DirectionalLight();
+            this.lightB.color            = 0xFF0000;
+            this.lightB.direction        = new away.geom.Vector3D( -1 , 0 ,1 );
+            this.lightB.ambient          = 0;//0.05;//.4;
+            this.lightB.ambientColor     = 0xFFFFFF;
+            this.lightB.diffuse          = 1;
+            this.lightB.specular         = 1;
+
+            this.staticLightPicker                              = new away.materials.StaticLightPicker( [this.light , this.lightB ] );
+
+            this.view.scene.addChild( this.light );
+            this.view.scene.addChild( this.lightB );
+
+            this.view.backgroundColor   = 0x222222;
+
+            window.onresize = () => this.resize();
+
+
+            this.initMeshes();
+            this.raf.start();
+            this.resize();
+        }
+
+        private initMeshes() : void
+        {
+
+            var primitives : Array<away.primitives.PrimitiveBase> = new Array<away.primitives.PrimitiveBase>();
+                primitives.push( new away.primitives.TorusGeometry() );
+                primitives.push( new away.primitives.SphereGeometry() );
+                primitives.push( new away.primitives.CapsuleGeometry() );
+                primitives.push( new away.primitives.CylinderGeometry() );
+                primitives.push( new away.primitives.PlaneGeometry() );
+                primitives.push( new away.primitives.ConeGeometry() );
+                primitives.push( new away.primitives.CubeGeometry() );
+
+            var mesh : away.entities.Mesh;
+
+            for ( var c: number = 0 ; c < primitives.length ; c ++ )
+            {
+
+                var t : number=Math.PI * 2 * c / primitives.length;
+
+                mesh = new away.entities.Mesh( primitives[c] )
+                mesh.x = Math.cos(t)*this.radius;
+                mesh.y = Math.sin(t)*this.radius;
+                mesh.z = 0;
+                mesh.scale( 2 );
+                mesh.material.lightPicker = this.staticLightPicker;
+
+                this.view.scene.addChild( mesh );
+                this.meshes.push( mesh );
+            }
+
+
+        }
+
+        private render()
+        {
+            if( this.meshes )
+            {
+                for ( var c : number = 0 ; c < this.meshes.length ; c++ )
+                {
+                    this.meshes[c].rotationY += 1;
+                }
+            }
+
+            this.view.render();
+
+        }
+
+        public resize()
+        {
+            this.view.y         = 0;
+            this.view.x         = 0;
+
+            this.view.width     = window.innerWidth;
+            this.view.height    = window.innerHeight;
+
 
         }
 
     }
 
 }
-
-window.onload = function ()
-{
-
-    var test = new tests.PrimitivesTest();
-
-
-}
-
-
--- tests/away/primitives/WireframePrimitive.ts
+++ tests/away/primitives/WireframePrimitive.ts
@@ -1,54 +1,119 @@
-///<reference path="../../../src/away/_definitions.ts" />
+///<reference path="../../../lib/Away3D.next.d.ts" />
+//<reference path="../../../src/Away3D.ts" />
 
 module tests.primitives
 {
 
-	export class WireframePrimitiveTest
-	{
+    export class WireframePrimitiveTest//extends away.events.EventDispatcher
+    {
 
-		private _view			: away.containers.View3D;
-        private _cubePrim       : away.primitives.WireframeCube;
-        private _spherePrim     : away.primitives.WireframeSphere
-        private _raf            : away.utils.RequestAnimationFrame;
+        private view                : away.containers.View3D;
+        private raf                 : away.utils.RequestAnimationFrame;
+        private meshes              : Array<away.primitives.WireframePrimitiveBase> = new Array<away.primitives.WireframePrimitiveBase>();
+        private light               : away.lights.DirectionalLight;
+        private lightB              : away.lights.DirectionalLight;
+        private staticLightPicker   : away.materials.StaticLightPicker;
+        //private t                   : number = 0;
+        private radius              : number = 400;
 
-		constructor()
-		{
 
-			this._view                  = new away.containers.View3D( );
-			this._view.backgroundColor  = 0x000000;
-			this._view.camera.lens      = new away.cameras.PerspectiveLens( 60 );
-			this._cubePrim              = new away.primitives.WireframeCube( 400 , 400 , 400 );
+        constructor()
+        {
 
-            this._spherePrim            = new away.primitives.WireframeSphere( 500 , 16 , 16 , 0x999999,.5);
+            away.Debug.LOG_PI_ERRORS    = false;
+            away.Debug.THROW_ERRORS     = false;
 
-            this._view.scene.addChild( this._cubePrim );
-            //this._view.scene.addChild( this._spherePrim );
+            this.view                   = new away.containers.View3D( );
+            this.raf                    = new away.utils.RequestAnimationFrame( this.render , this );
 
-            this._raf = new away.utils.RequestAnimationFrame( this.render , this );
-            this._raf.start();
+            this.light                  = new away.lights.DirectionalLight();
+            this.light.color            = 0xFFFFFF;
+            this.light.direction        = new away.geom.Vector3D( 1 , 1 ,0 );
+            this.light.ambient          = 0;//0.05;//.4;
+            this.light.ambientColor     = 0xFFFFFF;
+            this.light.diffuse          = 1;
+            this.light.specular         = 1;
 
-            window.onmousedown = () => this.render();
+            this.lightB                  = new away.lights.DirectionalLight();
+            this.lightB.color            = 0xFF0000;
+            this.lightB.direction        = new away.geom.Vector3D( -1 , 0 ,1 );
+            this.lightB.ambient          = 0;//0.05;//.4;
+            this.lightB.ambientColor     = 0xFFFFFF;
+            this.lightB.diffuse          = 1;
+            this.lightB.specular         = 1;
+
+            this.staticLightPicker                              = new away.materials.StaticLightPicker( [this.light , this.lightB ] );
+
+            this.view.scene.addChild( this.light );
+            this.view.scene.addChild( this.lightB );
+
+            this.view.backgroundColor   = 0x222222;
+
             window.onresize = () => this.resize();
 
+
+            this.initMeshes();
+            this.raf.start();
             this.resize();
+        }
 
-		}
+        private initMeshes() : void
+        {
 
-		public render( dt:number = null ):void
-		{
-            this._view.render();
-            this._cubePrim.rotationY ++;
-           // this._spherePrim.rotationY --;
-		}
+            var primitives : Array<away.primitives.WireframePrimitiveBase> = new Array<away.primitives.WireframePrimitiveBase>();
+            primitives.push( new away.primitives.WireframeCube() );
+            primitives.push( new away.primitives.WireframePlane( 200 , 200 ) );
+            primitives.push( new away.primitives.WireframeCylinder() );
+            primitives.push( new away.primitives.WireframeTetrahedron(200 , 200) );
+            primitives.push( new away.primitives.WireframeRegularPolygon(200 , 20) );
+
+
+            var mesh : away.entities.Mesh;
+
+            for ( var c: number = 0 ; c < primitives.length ; c ++ )
+            {
+
+                var t : number=Math.PI * 2 * c / primitives.length;
+
+                primitives[c].x = Math.cos(t)*this.radius;
+                primitives[c].y = Math.sin(t)*this.radius;
+                primitives[c].z = 0;
+                primitives[c].scale( 2 );
+                //mesh.material.lightPicker = this.staticLightPicker;
+
+                this.view.scene.addChild( primitives[c] );
+                this.meshes.push( primitives[c] );
+            }
+
+
+        }
+
+        private render()
+        {
+            if( this.meshes )
+            {
+                for ( var c : number = 0 ; c < this.meshes.length ; c++ )
+                {
+                    this.meshes[c].rotationY += 1;
+                }
+            }
+
+            this.view.render();
+
+        }
 
         public resize()
         {
-            this._view.y         = 0;
-            this._view.x         = 0;
-            this._view.width     = window.innerWidth;
-            this._view.height    = window.innerHeight;
+            this.view.y         = 0;
+            this.view.x         = 0;
+
+            this.view.width     = window.innerWidth;
+            this.view.height    = window.innerHeight;
+
+
         }
 
-	}
+    }
 
 }
+
