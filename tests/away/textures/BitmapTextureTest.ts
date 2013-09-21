52a80b3bd67c5316011ab983a1a90ea0bb16419d
Karim Beyrouti <karim@kurst.co.uk>
2013-09-21 19:21:38 +0100

    updated texture tests

--- tests/away/textures/BitmapTextureTest.ts
+++ tests/away/textures/BitmapTextureTest.ts
@@ -1,60 +1,45 @@
-///<reference path="../../../src/away/_definitions.ts" />
+///<reference path="../../../lib/Away3D.next.d.ts" />
 
-//------------------------------------------------------------------------------------------------
-// Web / PHP Storm arguments string
-//------------------------------------------------------------------------------------------------
-// --sourcemap $ProjectFileDir$/tests/away/textures/BitmapTextureTest.ts --target ES5 --comments --out $ProjectFileDir$/tests/away/textures/BitmapTextureTest.js
-//------------------------------------------------------------------------------------------------
-
-class BitmapTextureTest
+module tests.textures
 {
-
-    private mipLoader       : away.net.IMGLoader;
-    private bitmapData      : away.display.BitmapData;
-    private target          : away.textures.BitmapTexture;
-    private texture         : away.display3D.Texture;
-
-    constructor()
+    export class BitmapTextureTest
     {
 
-        //---------------------------------------
-        // Load a PNG
+        private mipLoader       : away.net.IMGLoader;
+        private bitmapData      : away.display.BitmapData;
+        private target          : away.textures.BitmapTexture;
+        private texture         : away.display3D.Texture;
 
-        var mipUrlRequest = new away.net.URLRequest( '../../assets/1024x1024.png');
-        this.mipLoader  = new away.net.IMGLoader();
-        this.mipLoader.load( mipUrlRequest );
-        this.mipLoader.addEventListener( away.events.Event.COMPLETE , this.mipImgLoaded , this );
+        constructor()
+        {
+
+            //---------------------------------------
+            // Load a PNG
+
+            var mipUrlRequest = new away.net.URLRequest( 'assets/1024x1024.png');
+            this.mipLoader  = new away.net.IMGLoader();
+            this.mipLoader.load( mipUrlRequest );
+            this.mipLoader.addEventListener( away.events.Event.COMPLETE , this.mipImgLoaded , this );
+
+        }
+
+        private mipImgLoaded( e )
+        {
+
+            var loader  : away.net.IMGLoader        = <away.net.IMGLoader > e.target;
+            var rect    : away.geom.Rectangle       = new away.geom.Rectangle( 0 , 0 , this.mipLoader.width , this.mipLoader.height );
+
+            console.log( 'away.events.Event.COMPLETE' , loader );
+
+            this.bitmapData                         = new away.display.BitmapData( loader.width , loader.height );
+            this.bitmapData.drawImage( this.mipLoader.image , rect ,  rect );
+
+            this.target                             = new away.textures.BitmapTexture( this.bitmapData , true );//new away.textures.HTMLImageElementTexture( loader.image , false );
+
+            away.Debug.log( 'away.display.BitmapData'           , this.bitmapData );
+            away.Debug.log( 'away.textures.BitmapTexture'       , this.target );
+
+        }
 
     }
-
-    private mipImgLoaded( e )
-    {
-
-        var loader : away.net.IMGLoader     = <away.net.IMGLoader > e.target;
-        var rect : away.geom.Rectangle      = new away.geom.Rectangle( 0 , 0 , this.mipLoader.width , this.mipLoader.height );
-
-        this.bitmapData                     = new away.display.BitmapData( loader.width , loader.height );
-        this.bitmapData.copyImage( this.mipLoader.image , rect ,  rect );
-
-        //this.texture        = new away.display3D.Texture( loader.width , loader.height );
-        this.target         = new away.textures.BitmapTexture( this.bitmapData , true );//new away.textures.HTMLImageElementTexture( loader.image , false );
-
-        away.Debug.log( 'bitmapData' , this.bitmapData );
-        //away.Debug.log( 'texture'    , this.texture );
-        away.Debug.log( 'target'     , this.target );
-
-    }
-
-}
-
-var GL = null;//: WebGLRenderingContext;
-var test: BitmapTextureTest;
-window.onload = function ()
-{
-
-    var canvas : HTMLCanvasElement = document.createElement('canvas');
-    GL = canvas.getContext("experimental-webgl");
-     test = new BitmapTextureTest();
-
-
 }
--- tests/away/textures/HTMLImageElementTextureTest.ts
+++ tests/away/textures/HTMLImageElementTextureTest.ts
@@ -1,53 +1,49 @@
-///<reference path="../../../src/away/_definitions.ts" />
+///<reference path="../../../lib/Away3D.next.d.ts" />
+//<reference path="../../../src/Away3D.ts" />
 
-//------------------------------------------------------------------------------------------------
-// Web / PHP Storm arguments string
-//------------------------------------------------------------------------------------------------
-// --sourcemap $ProjectFileDir$/tests/away/textures/HTMLImageElementTextureTest.ts --target ES5 --comments --out $ProjectFileDir$/tests/away/textures/HTMLImageElementTextureTest.js
-//------------------------------------------------------------------------------------------------
-
-class HTMLImageElementTextureTest
+module tests.textures
 {
 
-    private mipLoader       : away.net.IMGLoader;
-    private target          : away.textures.HTMLImageElementTexture;
-    private texture         : away.display3D.Texture;
-
-    constructor()
+    export class HTMLImageElementTextureTest
     {
 
-        //---------------------------------------
-        // Load a PNG
+        private mipLoader       : away.net.IMGLoader;
+        private target          : away.textures.HTMLImageElementTexture;
+        private texture         : away.display3D.Texture;
 
-        var mipUrlRequest = new away.net.URLRequest( '../../assets/1024x1024.png');
-        this.mipLoader  = new away.net.IMGLoader();
-        this.mipLoader.load( mipUrlRequest );
-        this.mipLoader.addEventListener( away.events.Event.COMPLETE , this.mipImgLoaded , this );
+        constructor()
+        {
+
+            //---------------------------------------
+            // Load a PNG
+
+            var mipUrlRequest = new away.net.URLRequest( 'assets/1024x1024.png');
+            this.mipLoader  = new away.net.IMGLoader();
+            this.mipLoader.load( mipUrlRequest );
+            this.mipLoader.addEventListener( away.events.Event.COMPLETE , this.mipImgLoaded , this );
+
+        }
+
+        private mipImgLoaded( e )
+        {
+
+            var stage       : away.display.Stage        = new away.display.Stage();
+            var stage3D     : away.display.Stage3D      = stage.getStage3DAt( 0 );
+            var context3D   : away.display3D.Context3D  = new away.display3D.Context3D( stage3D.canvas );
+            var loader      : away.net.IMGLoader        = <away.net.IMGLoader > e.target;
+
+            console.log( 'away.events.Event.COMPLETE' , loader );
+
+            this.texture    = new away.display3D.Texture( context3D._gl , loader.width , loader.height );
+            this.target     = new away.textures.HTMLImageElementTexture( loader.image , false );
+
+            console.log( 'away.display3D.Texture - Created' , this.texture );
+            console.log( 'away.textures.HTMLImageElementTexture - Created' , this.target );
+
+            away.materials.MipmapGenerator.generateHTMLImageElementMipMaps( this.target.htmlImageElement , <away.display3D.TextureBase> this.texture );
+
+        }
 
     }
-
-    private mipImgLoaded( e )
-    {
-
-        var loader : away.net.IMGLoader             = <away.net.IMGLoader > e.target;
-
-        this.texture = new away.display3D.Texture( loader.width , loader.height );
-        this.target = new away.textures.HTMLImageElementTexture( loader.image , false );
-
-        away.materials.MipmapGenerator.generateHTMLImageElementMipMaps( this.target.htmlImageElement , <away.display3D.TextureBase> this.texture );
-
-    }
-
-}
-
-var GL = null;//: WebGLRenderingContext;
-var test: HTMLImageElementTextureTest;
-window.onload = function ()
-{
-
-    var canvas : HTMLCanvasElement = document.createElement('canvas');
-    GL = canvas.getContext("experimental-webgl");
-     test = new HTMLImageElementTextureTest();
-
 
 }
