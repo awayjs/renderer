71337224d3ffbe5cb9fe3eeb1dcb56d0a62c9f76
Karim Beyrouti <karim@kurst.co.uk>
2013-09-21 21:42:04 +0100

    Updated test to work with compiled Away3D JavaScript / TypeScript definitions file and new app harness. Removed JS / HTML

--- tests/away/managers/ManagersTest.ts
+++ tests/away/managers/ManagersTest.ts
@@ -1,83 +1,72 @@
-///<reference path="../../../src/away/_definitions.ts" />
+///<reference path="../../../lib/Away3D.next.d.ts" />
+//<reference path="../../../src/Away3D.ts" />
 
-//------------------------------------------------------------------------------------------------
-// Web / PHP Storm arguments string
-//------------------------------------------------------------------------------------------------
-// --sourcemap $ProjectFileDir$/tests/away/managers/ManagersTest.ts --target ES5 --comments --out $ProjectFileDir$/tests/away/managers/ManagersTest.js
-//------------------------------------------------------------------------------------------------
 
-class ManagersTest
+
+module tests.managers
 {
 
-    private stage       : away.display.Stage;
-    private sManager    : away.managers.Stage3DManager;
-    private sProxy      : away.managers.Stage3DProxy;
-
-    private geomBase    : away.base.SubGeometryBase = new away.base.SubGeometryBase();
-    private geom        : away.base.Geometry = new away.base.Geometry();
-    private rttBfrA     : away.managers.RTTBufferManager;
-    private rttBfrB     : away.managers.RTTBufferManager;
-
-    constructor()
+    export class ManagersTest
     {
 
-        away.Debug.THROW_ERRORS = false;
+        private stage       : away.display.Stage;
+        private sManager    : away.managers.Stage3DManager;
+        private sProxy      : away.managers.Stage3DProxy;
 
-        this.stage = new away.display.Stage();
+        private geomBase    : away.base.SubGeometryBase = new away.base.SubGeometryBase();
+        private geom        : away.base.Geometry = new away.base.Geometry();
+        private rttBfrA     : away.managers.RTTBufferManager;
+        private rttBfrB     : away.managers.RTTBufferManager;
 
-        var manager : away.managers.Stage3DManager = away.managers.Stage3DManager.getInstance( this.stage );
+        constructor()
+        {
 
-        this.sProxy = manager.getStage3DProxy( 0 );
-        this.sProxy.addEventListener( away.events.Stage3DEvent.CONTEXT3D_CREATED , this.onContextCreated , this );
-        this.sProxy.addEventListener( away.events.Stage3DEvent.CONTEXT3D_RECREATED, this.onContextReCreated , this );
-        this.sProxy.addEventListener( away.events.Stage3DEvent.CONTEXT3D_DISPOSED, this.onContextDisposed , this );
+            away.Debug.THROW_ERRORS = false;
 
-        this.rttBfrA = away.managers.RTTBufferManager.getInstance( this.sProxy )
-        this.rttBfrB = away.managers.RTTBufferManager.getInstance( this.sProxy )
+            this.stage = new away.display.Stage();
 
-        console.log( 'this.rttBfrA' , this.rttBfrA );
-        console.log( 'this.rttBfrB' , this.rttBfrB );
+            var manager : away.managers.Stage3DManager = away.managers.Stage3DManager.getInstance( this.stage );
 
-        this.rttBfrB.dispose();
+            this.sProxy = manager.getStage3DProxy( 0 );
+            this.sProxy.addEventListener( away.events.Stage3DEvent.CONTEXT3D_CREATED , this.onContextCreated , this );
+            this.sProxy.addEventListener( away.events.Stage3DEvent.CONTEXT3D_RECREATED, this.onContextReCreated , this );
+            this.sProxy.addEventListener( away.events.Stage3DEvent.CONTEXT3D_DISPOSED, this.onContextDisposed , this );
 
-        console.log( 'this.rttBfrA' , this.rttBfrA );
-        console.log( 'this.rttBfrB' , this.rttBfrB );
+            this.rttBfrA = away.managers.RTTBufferManager.getInstance( this.sProxy )
+            this.rttBfrB = away.managers.RTTBufferManager.getInstance( this.sProxy )
+
+            console.log( 'this.rttBfrA' , this.rttBfrA );
+            console.log( 'this.rttBfrB' , this.rttBfrB );
+
+            this.rttBfrB.dispose();
+
+            console.log( 'this.rttBfrA' , this.rttBfrA );
+            console.log( 'this.rttBfrB' , this.rttBfrB );
 
 
+
+        }
+
+        public onContextCreated( e : away.events.Stage3DEvent ) : void
+        {
+
+            away.Debug.log( 'onContextCreated' , e );
+
+        }
+
+        public onContextReCreated( e : away.events.Stage3DEvent ) : void
+        {
+
+            away.Debug.log( 'onContextReCreated' , e );
+
+        }
+
+        public onContextDisposed( e : away.events.Stage3DEvent ) : void
+        {
+
+            away.Debug.log( 'onContextDisposed' , e );
+
+        }
 
     }
-
-    public onContextCreated( e : away.events.Stage3DEvent ) : void
-    {
-
-        away.Debug.log( 'onContextCreated' , e );
-
-    }
-
-    public onContextReCreated( e : away.events.Stage3DEvent ) : void
-    {
-
-        away.Debug.log( 'onContextReCreated' , e );
-
-    }
-
-    public onContextDisposed( e : away.events.Stage3DEvent ) : void
-    {
-
-        away.Debug.log( 'onContextDisposed' , e );
-
-    }
-
-}
-
-var GL = null;//: WebGLRenderingContext;
-var test: ManagersTest;
-window.onload = function ()
-{
-
-    var canvas : HTMLCanvasElement = document.createElement('canvas');
-    GL = canvas.getContext("experimental-webgl");
-
-    test = new ManagersTest();
-
 }
