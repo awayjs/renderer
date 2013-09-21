00daf55e15f34cdb965b3b6b100d14595f917a00
Karim Beyrouti <karim@kurst.co.uk>
2013-09-21 22:15:35 +0100

    Updated test to work with compiled Away3D JavaScript / TypeScript definitions file and new app harness. Removed JS / HTML

--- tests/away/geom/ColorUtils.ts
+++ tests/away/geom/ColorUtils.ts
@@ -1,79 +1,65 @@
-///<reference path="../../../src/away/geom/ColorTransform.ts" />
-///<reference path="../../../src/away/utils/ColorUtils.ts" />
+///<reference path="../loaders/parsers/JSONTextureParser.ts" />
+///<reference path="../../../lib/Away3D.next.d.ts" />
+//<reference path="../../../src/Away3D.ts" />
 
-//------------------------------------------------------------------------------------------------
-// Web / PHP Storm arguments string
-//------------------------------------------------------------------------------------------------
-// --sourcemap $ProjectFileDir$/tests/away/geom/ColorUtils.ts --target ES5 --comments --out $ProjectFileDir$/tests/away/geom/ColorUtils.js
-//------------------------------------------------------------------------------------------------
+module tests.geom {
 
-
-class ColorUtils
-{
-
-    constructor()
+    export class ColorUtils
     {
 
-        /*
-        constructor(    inRedMultiplier:number = 1.0,  inGreenMultiplier:number = 1.0, inBlueMultiplier:number = 1.0,  inAlphaMultiplier:number = 1.0,
-                        inRedOffset:number = 0.0,      inGreenOffset:number = 0.0,     inBlueOffset:number = 0.0,      inAlphaOffset:number = 0.0)
-        */
+        constructor()
+        {
 
-        var ct_RED = new away.geom.ColorTransform( 1   , 0 , 0 , 1 ,
-            255 , 0 , 0 , 255 );
+            /*
+            constructor(    inRedMultiplier:number = 1.0,  inGreenMultiplier:number = 1.0, inBlueMultiplier:number = 1.0,  inAlphaMultiplier:number = 1.0,
+                            inRedOffset:number = 0.0,      inGreenOffset:number = 0.0,     inBlueOffset:number = 0.0,      inAlphaOffset:number = 0.0)
+            */
 
-        console.log( "ct_RED - ARGB: ", away.utils.ColorUtils.float32ColorToARGB( ct_RED.color ));
+            var ct_RED = new away.geom.ColorTransform( 1   , 0 , 0 , 1 ,
+                255 , 0 , 0 , 255 );
 
-        var ct_GREEN = new away.geom.ColorTransform( 0   , 1   , 0 , 1 ,
-            0   , 255 , 0 , 255 );
+            console.log( "ct_RED - ARGB: ", away.utils.ColorUtils.float32ColorToARGB( ct_RED.color ));
 
-        console.log( "ct_GREEN - ARGB: ", away.utils.ColorUtils.float32ColorToARGB( ct_GREEN.color ));
+            var ct_GREEN = new away.geom.ColorTransform( 0   , 1   , 0 , 1 ,
+                0   , 255 , 0 , 255 );
 
-        var ct_BLUE = new away.geom.ColorTransform( 0   , 0   , 1   , 1 ,
-            0   , 0   , 255 , 255 );
+            console.log( "ct_GREEN - ARGB: ", away.utils.ColorUtils.float32ColorToARGB( ct_GREEN.color ));
 
-        console.log( "ct_BLUE - ARGB: ", away.utils.ColorUtils.float32ColorToARGB( ct_BLUE.color ));
+            var ct_BLUE = new away.geom.ColorTransform( 0   , 0   , 1   , 1 ,
+                0   , 0   , 255 , 255 );
 
-        var ct_RED_a = new away.geom.ColorTransform(.5   , 0 , 0 , 1 ,
-                                                     255 , 0 , 0 , 255 );
+            console.log( "ct_BLUE - ARGB: ", away.utils.ColorUtils.float32ColorToARGB( ct_BLUE.color ));
 
-        console.log( "ct_RED_a - ARGB: ", away.utils.ColorUtils.float32ColorToARGB( ct_RED_a.color ));
+            var ct_RED_a = new away.geom.ColorTransform(.5   , 0 , 0 , 1 ,
+                                                         255 , 0 , 0 , 255 );
 
-        var ct_GREEN_a = new away.geom.ColorTransform( 0   ,.5   , 0 , 1 ,
-                                                       0   , 255 , 0 , 255 );
+            console.log( "ct_RED_a - ARGB: ", away.utils.ColorUtils.float32ColorToARGB( ct_RED_a.color ));
 
-        console.log( "ct_GREEN_a - ARGB: ", away.utils.ColorUtils.float32ColorToARGB( ct_GREEN_a.color ));
+            var ct_GREEN_a = new away.geom.ColorTransform( 0   ,.5   , 0 , 1 ,
+                                                           0   , 255 , 0 , 255 );
 
-        var ct_BLUE_a = new away.geom.ColorTransform( 0   , 0   , .5  , 1 ,
-                                                      0   , 0   , 255 , 255 );
+            console.log( "ct_GREEN_a - ARGB: ", away.utils.ColorUtils.float32ColorToARGB( ct_GREEN_a.color ));
 
-        console.log( "ct_BLUE_a - ARGB: ", away.utils.ColorUtils.float32ColorToARGB( ct_BLUE_a.color ));
+            var ct_BLUE_a = new away.geom.ColorTransform( 0   , 0   , .5  , 1 ,
+                                                          0   , 0   , 255 , 255 );
+
+            console.log( "ct_BLUE_a - ARGB: ", away.utils.ColorUtils.float32ColorToARGB( ct_BLUE_a.color ));
 
 
-        console.log('--------------------------------------------------------------------------------');
+            console.log('--------------------------------------------------------------------------------');
 
-        ct_RED.color = 0xff0000;
-        console.log( "SET - ct_RED - ARGB: ", away.utils.ColorUtils.float32ColorToARGB( ct_RED.color ));
+            ct_RED.color = 0xff0000;
+            console.log( "SET - ct_RED - ARGB: ", away.utils.ColorUtils.float32ColorToARGB( ct_RED.color ));
 
-        ct_GREEN.color = 0x00ff00;
-        console.log( "SET - ct_GREEN - ARGB: ", away.utils.ColorUtils.float32ColorToARGB( ct_GREEN.color ));
+            ct_GREEN.color = 0x00ff00;
+            console.log( "SET - ct_GREEN - ARGB: ", away.utils.ColorUtils.float32ColorToARGB( ct_GREEN.color ));
 
-        ct_BLUE.color = 0x0000ff;
-        console.log( "SET - ct_BLUE - ARGB: ", away.utils.ColorUtils.float32ColorToARGB( ct_BLUE.color ));
+            ct_BLUE.color = 0x0000ff;
+            console.log( "SET - ct_BLUE - ARGB: ", away.utils.ColorUtils.float32ColorToARGB( ct_BLUE.color ));
+
+
+        }
 
 
     }
-
-
 }
-
-
-window.onload = function ()
-{
-
-
-    var test = new ColorUtils();
-
-
-}
-
--- tests/away/geom/MatrixTest.ts
+++ tests/away/geom/MatrixTest.ts
@@ -1,50 +1,23 @@
-///<reference path="../../../src/away/_definitions.ts" />
+///<reference path="../loaders/parsers/JSONTextureParser.ts" />
+///<reference path="../../../lib/Away3D.next.d.ts" />
+//<reference path="../../../src/Away3D.ts" />
 
-//------------------------------------------------------------------------------------------------
-// Web / PHP Storm arguments string
-//------------------------------------------------------------------------------------------------
-// --sourcemap $ProjectFileDir$/tests/away/geom/MatrixTest.ts --target ES5 --comments --out $ProjectFileDir$/tests/away/geom/MatrixTest.js
-//------------------------------------------------------------------------------------------------
+module tests.geom {
 
-
-class MatrixTest
-{
-
-    private ma : away.geom.Matrix = new away.geom.Matrix( 10 , 11, 12 , 13 , 14 , 15);
-    private mb : away.geom.Matrix = new away.geom.Matrix( 0 , 1, 2 , 3 , 4 , 5);
-
-    constructor()
+    export class MatrixTest
     {
 
-        this.ma.concat( this.mb );
-        console.log( this.ma );
+        private ma : away.geom.Matrix = new away.geom.Matrix( 10 , 11, 12 , 13 , 14 , 15);
+        private mb : away.geom.Matrix = new away.geom.Matrix( 0 , 1, 2 , 3 , 4 , 5);
+
+        constructor()
+        {
+
+            this.ma.concat( this.mb );
+            console.log( this.ma );
+
+        }
+
 
     }
-
-
 }
-
-var GL = null;//: WebGLRenderingContext;
-
-window.onload = function ()
-{
-
-    var canvas : HTMLCanvasElement = document.createElement('canvas');
-    GL = canvas.getContext("experimental-webgl");
-
-    try
-    {
-
-        var test = new MatrixTest();
-
-    }
-    catch ( e )
-    {
-
-        console.log( e );
-
-    }
-
-
-}
-
