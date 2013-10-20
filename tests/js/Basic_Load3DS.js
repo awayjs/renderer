f37ba2d036701e2140a622aac2e5bf231cf38a73
Karim Beyrouti <karim@kurst.co.uk>
2013-10-21 00:10:25 +0100

    Basic_Load3DS - added drop down selector to change ShadowMethods

--- tests/js/Basic_Load3DS.js
+++ tests/js/Basic_Load3DS.js
@@ -86,7 +86,7 @@ var examples;
         */
         Basic_Load3DS.prototype.initMaterials = function () {
             this._groundMaterial = new away.materials.TextureMaterial();
-            this._groundMaterial.shadowMethod = new away.materials.FilteredShadowMapMethod(this._light);
+            this._groundMaterial.shadowMethod = new away.materials.SoftShadowMapMethod(this._light, 10, 5);
             this._groundMaterial.shadowMethod.epsilon = 0.2;
             this._groundMaterial.lightPicker = this._lightPicker;
             this._groundMaterial.specular = 0;
@@ -169,13 +169,16 @@ var examples;
                     break;
                 case away.library.AssetType.MATERIAL:
                     var material = event.asset;
-                    material.shadowMethod = new away.materials.FilteredShadowMapMethod(this._light);
+                    material.shadowMethod = new away.materials.SoftShadowMapMethod(this._light, 10, 5);
                     material.shadowMethod.epsilon = 0.2;
                     material.lightPicker = this._lightPicker;
                     material.gloss = 30;
                     material.specular = 1;
                     material.ambientColor = 0x303040;
                     material.ambient = 1;
+
+                    this.initInterface();
+
                     break;
             }
         };
@@ -234,6 +237,59 @@ var examples;
             this._view.x = 0;
             this._view.width = window.innerWidth;
             this._view.height = window.innerHeight;
+        };
+
+        /**
+        * Test interface to swap ShadowMapMethod
+        */
+        Basic_Load3DS.prototype.initInterface = function () {
+            var _this = this;
+            var testDiv = document.createElement('div');
+            testDiv.style.cssFloat = 'none';
+            testDiv.style.position = 'absolute';
+            testDiv.style.top = '15px';
+            testDiv.style.width = '600px';
+            testDiv.style.left = '50%';
+            testDiv.style.marginLeft = '-300px';
+            testDiv.style.textAlign = 'center';
+
+            this.dropDown = document.createElement('select');
+            this.dropDown.name = "selectTestDropDown";
+            this.dropDown.id = "selectTest";
+
+            testDiv.appendChild(this.dropDown);
+
+            var option = new Option('FilteredShadowMapMethod', 'FilteredShadowMapMethod');
+            this.dropDown.add(option);
+
+            option = new Option('SoftShadowMapMethod', 'SoftShadowMapMethod');
+            this.dropDown.add(option);
+
+            option = new Option('DitheredShadowMapMethod', 'DitheredShadowMapMethod');
+            this.dropDown.add(option);
+
+            document.body.appendChild(testDiv);
+
+            this.dropDown.onchange = function (e) {
+                return _this.dropDownChange(e);
+            };
+        };
+
+        Basic_Load3DS.prototype.dropDownChange = function (e) {
+            switch (this.dropDown.options[this.dropDown.selectedIndex].value) {
+                case 'FilteredShadowMapMethod':
+                    this._groundMaterial.shadowMethod = new away.materials.FilteredShadowMapMethod(this._light);
+                    break;
+
+                case 'SoftShadowMapMethod':
+                    this._groundMaterial.shadowMethod = new away.materials.SoftShadowMapMethod(this._light, 10, 5);
+                    break;
+
+                case 'DitheredShadowMapMethod':
+                    this._groundMaterial.shadowMethod = new away.materials.DitheredShadowMapMethod(this._light, 10, 5);
+                    break;
+            }
+            //var dataIndex : number = parseInt( this.dropDown.options[this.dropDown.selectedIndex].value ) ;
         };
         return Basic_Load3DS;
     })();
