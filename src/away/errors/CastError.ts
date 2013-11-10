dcbec885d9d68115a79f53700c08d8486e9f36c9
rob-bateman <rob@infiniteturtles.co.uk>
2013-11-10 22:59:49 +0000

    added missing CastError class

---
+++ src/away/errors/CastError.ts
@@ -0,0 +1,13 @@
+///<reference path="../_definitions.ts"/>
+
+module away.errors
+{
+
+    export class CastError extends Error
+    {
+        constructor(message:string)
+        {
+            super(message);
+        }
+    }
+}
