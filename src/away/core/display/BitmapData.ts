7f051c63852b54b6ab579d534d6d41fe0ee8866c
rob-bateman <rob@infiniteturtles.co.uk>
2013-11-10 23:21:02 +0000

    added colorTransform to BitmapData

--- src/away/core/display/BitmapData.ts
+++ src/away/core/display/BitmapData.ts
@@ -486,6 +486,37 @@ module away.display {
             }
         }
 
+        public colorTransform(rect: away.geom.Rectangle, colorTransform: away.geom.ColorTransform) : void
+        {
+            if ( ! this._locked )
+            {
+                this._imageData = this._context.getImageData(0,0,this._rect.width,this._rect.height);
+            }
+
+            if ( this._imageData )
+            {
+                var data:number[] = this._imageData.data;
+
+                var i:number /*uint*/, j:number /*uint*/, index : number /*uint*/;
+                for (i = 0; i < rect.width; ++i) {
+                    for (j = 0; j < rect.height; ++j) {
+                        index = (i + rect.x + (j + rect.y)*this.width) * 4;
+
+                        data[index] = data[index]*colorTransform.redMultiplier + colorTransform.redOffset;
+                        data[index + 1] = data[index + 1]*colorTransform.greenMultiplier + colorTransform.greenOffset;
+                        data[index + 2] = data[index + 2]*colorTransform.blueMultiplier + colorTransform.blueOffset;
+                        data[index + 3] = data[index + 3]*colorTransform.alphaMultiplier + colorTransform.alphaOffset;
+                    }
+                }
+            }
+
+            if ( ! this._locked )
+            {
+                this._context.putImageData( this._imageData, 0, 0);
+                this._imageData = null;
+            }
+        }
+
         /**
          *
          * @param {ImageData}
