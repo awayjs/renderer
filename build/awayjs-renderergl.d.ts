0382b9046fba04880732f1e7c4de1fac3dc99efd
rob-bateman <rob@infiniteturtles.co.uk>
2015-08-17 16:14:45 +0100

    update build files

--- build/awayjs-renderergl.d.ts
+++ build/awayjs-renderergl.d.ts
@@ -1435,7 +1435,7 @@ declare module "awayjs-renderergl/lib/animators/data/SkeletonJoint" {
 	    /**
 	     * The inverse bind pose matrix, as raw data, used to transform vertices to bind joint space in preparation for transformation using the joint matrix.
 	     */
-	    inverseBindPose: Array<number>;
+	    inverseBindPose: Float32Array;
 	    /**
 	     * Creates a new <code>SkeletonJoint</code> object
 	     */
@@ -2293,6 +2293,83 @@ declare module "awayjs-renderergl/lib/animators/nodes/ParticleSegmentedColorNode
 	
 }
 
+declare module "awayjs-renderergl/lib/animators/nodes/ParticleSpriteSheetNode" {
+	import AnimatorBase = require("awayjs-renderergl/lib/animators/AnimatorBase");
+	import AnimationRegisterCache = require("awayjs-renderergl/lib/animators/data/AnimationRegisterCache");
+	import ShaderBase = require("awayjs-renderergl/lib/shaders/ShaderBase");
+	import ParticleAnimationSet = require("awayjs-renderergl/lib/animators/ParticleAnimationSet");
+	import ParticleProperties = require("awayjs-renderergl/lib/animators/data/ParticleProperties");
+	import ParticleNodeBase = require("awayjs-renderergl/lib/animators/nodes/ParticleNodeBase");
+	import ParticleSpriteSheetState = require("awayjs-renderergl/lib/animators/states/ParticleSpriteSheetState");
+	/**
+	 * A particle animation node used when a spritesheet texture is required to animate the particle.
+	 * NB: to enable use of this node, the <code>repeat</code> property on the material has to be set to true.
+	 */
+	class ParticleSpriteSheetNode extends ParticleNodeBase {
+	    /** @private */
+	    _iUsesCycle: boolean;
+	    /** @private */
+	    _iUsesPhase: boolean;
+	    /** @private */
+	    _iTotalFrames: number;
+	    /** @private */
+	    _iNumColumns: number;
+	    /** @private */
+	    _iNumRows: number;
+	    /** @private */
+	    _iCycleDuration: number;
+	    /** @private */
+	    _iCyclePhase: number;
+	    /**
+	     * Reference for spritesheet node properties on a single particle (when in local property mode).
+	     * Expects a <code>Vector3D</code> representing the cycleDuration (x), optional phaseTime (y).
+	     */
+	    static UV_VECTOR3D: string;
+	    /**
+	     * Defines the number of columns in the spritesheet, when in global mode. Defaults to 1. Read only.
+	     */
+	    numColumns: number;
+	    /**
+	     * Defines the number of rows in the spritesheet, when in global mode. Defaults to 1. Read only.
+	     */
+	    numRows: number;
+	    /**
+	     * Defines the total number of frames used by the spritesheet, when in global mode. Defaults to the number defined by numColumns and numRows. Read only.
+	     */
+	    totalFrames: number;
+	    /**
+	     * Creates a new <code>ParticleSpriteSheetNode</code>
+	     *
+	     * @param               mode            Defines whether the mode of operation acts on local properties of a particle or global properties of the node.
+	     * @param    [optional] numColumns      Defines the number of columns in the spritesheet, when in global mode. Defaults to 1.
+	     * @param    [optional] numRows         Defines the number of rows in the spritesheet, when in global mode. Defaults to 1.
+	     * @param    [optional] cycleDuration   Defines the default cycle duration in seconds, when in global mode. Defaults to 1.
+	     * @param    [optional] cyclePhase      Defines the default cycle phase, when in global mode. Defaults to 0.
+	     * @param    [optional] totalFrames     Defines the total number of frames used by the spritesheet, when in global mode. Defaults to the number defined by numColumns and numRows.
+	     * @param    [optional] looping         Defines whether the spritesheet animation is set to loop indefinitely. Defaults to true.
+	     */
+	    constructor(mode: number, usesCycle: boolean, usesPhase: boolean, numColumns?: number, numRows?: number, cycleDuration?: number, cyclePhase?: number, totalFrames?: number);
+	    /**
+	     * @inheritDoc
+	     */
+	    getAGALUVCode(shader: ShaderBase, animationRegisterCache: AnimationRegisterCache): string;
+	    /**
+	     * @inheritDoc
+	     */
+	    getAnimationState(animator: AnimatorBase): ParticleSpriteSheetState;
+	    /**
+	     * @inheritDoc
+	     */
+	    _iProcessAnimationSetting(particleAnimationSet: ParticleAnimationSet): void;
+	    /**
+	     * @inheritDoc
+	     */
+	    _iGeneratePropertyOfOneParticle(param: ParticleProperties): void;
+	}
+	export = ParticleSpriteSheetNode;
+	
+}
+
 declare module "awayjs-renderergl/lib/animators/nodes/ParticleTimeNode" {
 	import AnimatorBase = require("awayjs-renderergl/lib/animators/AnimatorBase");
 	import AnimationRegisterCache = require("awayjs-renderergl/lib/animators/data/AnimationRegisterCache");
@@ -2511,83 +2588,6 @@ declare module "awayjs-renderergl/lib/animators/nodes/SkeletonClipNode" {
 	    _pUpdateStitch(): void;
 	}
 	export = SkeletonClipNode;
-	
-}
-
-declare module "awayjs-renderergl/lib/animators/nodes/ParticleSpriteSheetNode" {
-	import AnimatorBase = require("awayjs-renderergl/lib/animators/AnimatorBase");
-	import AnimationRegisterCache = require("awayjs-renderergl/lib/animators/data/AnimationRegisterCache");
-	import ShaderBase = require("awayjs-renderergl/lib/shaders/ShaderBase");
-	import ParticleAnimationSet = require("awayjs-renderergl/lib/animators/ParticleAnimationSet");
-	import ParticleProperties = require("awayjs-renderergl/lib/animators/data/ParticleProperties");
-	import ParticleNodeBase = require("awayjs-renderergl/lib/animators/nodes/ParticleNodeBase");
-	import ParticleSpriteSheetState = require("awayjs-renderergl/lib/animators/states/ParticleSpriteSheetState");
-	/**
-	 * A particle animation node used when a spritesheet texture is required to animate the particle.
-	 * NB: to enable use of this node, the <code>repeat</code> property on the material has to be set to true.
-	 */
-	class ParticleSpriteSheetNode extends ParticleNodeBase {
-	    /** @private */
-	    _iUsesCycle: boolean;
-	    /** @private */
-	    _iUsesPhase: boolean;
-	    /** @private */
-	    _iTotalFrames: number;
-	    /** @private */
-	    _iNumColumns: number;
-	    /** @private */
-	    _iNumRows: number;
-	    /** @private */
-	    _iCycleDuration: number;
-	    /** @private */
-	    _iCyclePhase: number;
-	    /**
-	     * Reference for spritesheet node properties on a single particle (when in local property mode).
-	     * Expects a <code>Vector3D</code> representing the cycleDuration (x), optional phaseTime (y).
-	     */
-	    static UV_VECTOR3D: string;
-	    /**
-	     * Defines the number of columns in the spritesheet, when in global mode. Defaults to 1. Read only.
-	     */
-	    numColumns: number;
-	    /**
-	     * Defines the number of rows in the spritesheet, when in global mode. Defaults to 1. Read only.
-	     */
-	    numRows: number;
-	    /**
-	     * Defines the total number of frames used by the spritesheet, when in global mode. Defaults to the number defined by numColumns and numRows. Read only.
-	     */
-	    totalFrames: number;
-	    /**
-	     * Creates a new <code>ParticleSpriteSheetNode</code>
-	     *
-	     * @param               mode            Defines whether the mode of operation acts on local properties of a particle or global properties of the node.
-	     * @param    [optional] numColumns      Defines the number of columns in the spritesheet, when in global mode. Defaults to 1.
-	     * @param    [optional] numRows         Defines the number of rows in the spritesheet, when in global mode. Defaults to 1.
-	     * @param    [optional] cycleDuration   Defines the default cycle duration in seconds, when in global mode. Defaults to 1.
-	     * @param    [optional] cyclePhase      Defines the default cycle phase, when in global mode. Defaults to 0.
-	     * @param    [optional] totalFrames     Defines the total number of frames used by the spritesheet, when in global mode. Defaults to the number defined by numColumns and numRows.
-	     * @param    [optional] looping         Defines whether the spritesheet animation is set to loop indefinitely. Defaults to true.
-	     */
-	    constructor(mode: number, usesCycle: boolean, usesPhase: boolean, numColumns?: number, numRows?: number, cycleDuration?: number, cyclePhase?: number, totalFrames?: number);
-	    /**
-	     * @inheritDoc
-	     */
-	    getAGALUVCode(shader: ShaderBase, animationRegisterCache: AnimationRegisterCache): string;
-	    /**
-	     * @inheritDoc
-	     */
-	    getAnimationState(animator: AnimatorBase): ParticleSpriteSheetState;
-	    /**
-	     * @inheritDoc
-	     */
-	    _iProcessAnimationSetting(particleAnimationSet: ParticleAnimationSet): void;
-	    /**
-	     * @inheritDoc
-	     */
-	    _iGeneratePropertyOfOneParticle(param: ParticleProperties): void;
-	}
-	export = ParticleSpriteSheetNode;
 	
 }
 
