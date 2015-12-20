08c50751abd9281a542743496e02dd6a9c082353
rob-bateman <rob@infiniteturtles.co.uk>
2015-12-20 02:54:01 +0000

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -70,6 +70,7 @@ function browserifyShare(callback) {
     var b = browserify({
         debug: true,
         paths: ['../'],
+        entries: './lib/RendererGL.ts',
         cache: {},
         packageCache: {},
         fullPaths: true
