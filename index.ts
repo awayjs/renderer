ce1aa53c183f619d1fd01c43a3ceb1d531c5d080
80prozent <80prozent@differentdesign.de>
2018-05-30 10:05:18 +0200

    log version to console

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+console.log("AwayJS - Renderer - 0.9.11");
 export {IContainerNode} from "./lib/base/IContainerNode";
 export {IEntity} from "./lib/base/IEntity";
 export {IMaterial} from "./lib/base/IMaterial";
