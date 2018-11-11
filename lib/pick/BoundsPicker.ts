a38595ee7e5cc6e24869fdb0efe6d01229670873
Rob Bateman <rob@infiniteturtles.co.uk>
2018-11-11 22:28:08 +0000

    bounds calculations ignore the pickObject

--- lib/pick/BoundsPicker.ts
+++ lib/pick/BoundsPicker.ts
@@ -208,15 +208,7 @@ export class BoundsPicker extends AbstractionBase implements ITraverser, IBounds
 	 */
 	public enterNode(node:INode):boolean
 	{
-		if (!node.isVisible())
-			return false;
-
-		if (node.pickObject) {
-			node.pickObject.partition.traverse(this);
-			return false;
-		}
-
-		return true;
+		return node.isVisible();
 	}
 
 	public getBoundingVolume(targetCoordinateSpace:IEntity = null, boundingVolumeType:BoundingVolumeType = null):BoundingVolumeBase
