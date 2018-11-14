74e553ae189d96caae856280c02f5ac345a44a7a
80prozent <80prozent@differentdesign.de>
2018-11-14 02:15:51 +0100

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-console.log("AwayJS - Renderer - 0.9.23");
+console.log("AwayJS - Renderer - 0.9.24");
 export {IEntity} from "./lib/base/IEntity";
 export {IMaterial} from "./lib/base/IMaterial";
 export {IMaterialClass} from "./lib/base/IMaterialClass";
