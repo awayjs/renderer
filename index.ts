2d5929aa1b11d99379972542e3af055f8bd16177
Rob Bateman <rob.bateman@gmail.com>
2019-12-05 17:32:43 +0000

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-console.debug("AwayJS - Renderer - 0.10.7");
+console.debug("AwayJS - Renderer - 0.10.8");
 export {IRenderEntity} from "./lib/base/IRenderEntity";
 export {IMaterial} from "./lib/base/IMaterial";
 export {IMaterialClass} from "./lib/base/IMaterialClass";
