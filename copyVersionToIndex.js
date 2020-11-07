c1ee0b583cb9b7c19332c8bd9c0db7caac7e4ab3
Exponenta <rondo.devil@gmail.com>
2020-11-07 13:57:03 +0300

    Lint + change version script to allow '

--- copyVersionToIndex.js
+++ copyVersionToIndex.js
@@ -1,7 +1,6 @@
-"use strict";
+'use strict';
 exports.__esModule = true;
-var fs = require("fs");
-
+const fs = require('fs');
 
 // read in the /index.ts
 
@@ -9,16 +8,17 @@ var fs = require("fs");
 
 // update /index.ts with the new content
 
-console.log("update src/index.ts with version:", process.env.npm_package_version);
+console.log('update src/index.ts with version:', process.env.npm_package_version);
 
-fs.readFile("./index.ts", 'utf8', function(err, data) {
-    if (err) throw err;
-    var re = /(.*[a-zA-Z]\s\-\s)(.*)(\"\)\;.*)/;
-    //console.log("before", data)
-    data = data.replace(re, "$1"+process.env.npm_package_version+"$3");//#BUILD_VIA_NPM_VERSION_PATCH_TO_DISPLAY_VERSION_HERE#", process.env.npm_package_version);
-    //console.log("after", data)
-    fs.writeFile("./index.ts", data, function(err) {
-        if (err) throw err;    
-        console.log("Updated ./index.ts with inserted version ", process.env.npm_package_version);
-    }); 
-});           
+fs.readFile('./index.ts', 'utf8', function(err, data) {
+	if (err) throw err;
+	const re = /(.*[a-zA-Z]\s-\s)(.*)(["|']\);.*)/;
+	//console.log("before", data)
+	//#BUILD_VIA_NPM_VERSION_PATCH_TO_DISPLAY_VERSION_HERE#", process.env.npm_package_version);
+	data = data.replace(re, '$1' + process.env.npm_package_version + '$3');
+	//console.log("after", data)
+	fs.writeFile('./index.ts', data, function(err) {
+		if (err) throw err;
+		console.log('Updated ./index.ts with inserted version ', process.env.npm_package_version);
+	});
+});
--- index.ts
+++ index.ts
@@ -1,49 +1,52 @@
-console.debug("AwayJS - Renderer - 0.11.3");
-export {IRenderEntity} from "./lib/base/IRenderEntity";
-export {IMaterial} from "./lib/base/IMaterial";
-export {IMaterialClass} from "./lib/base/IMaterialClass";
-export {_IRender_ElementsClass} from "./lib/base/_IRender_ElementsClass";
-export {_IRender_MaterialClass} from "./lib/base/_IRender_MaterialClass";
-export {IPass} from "./lib/base/IPass";
-export {ITexture} from "./lib/base/ITexture";
-export {IElements} from "./lib/base/IElements";
-export {ShaderBase} from "./lib/base/ShaderBase";
-export {_IRender_RenderableClass} from "./lib/base/_IRender_RenderableClass";
-export {_Render_MaterialBase} from "./lib/base/_Render_MaterialBase";
-export {_Render_ElementsBase} from "./lib/base/_Render_ElementsBase";
-export {_Render_RenderableBase} from "./lib/base/_Render_RenderableBase";
-export {RenderEntity} from "./lib/base/RenderEntity";
-export {IAnimationSet} from "./lib/base/IAnimationSet";
-export {IAnimator} from "./lib/base/IAnimator";
-export {Style} from "./lib/base/Style";
-export {_Shader_TextureBase} from "./lib/base/_Shader_TextureBase";
-export {AnimationRegisterData} from "./lib/base/AnimationRegisterData";
-export {AnimationNodeBase}				from "./lib/base/AnimationNodeBase";
-export {_Stage_ElementsBase} from "./lib/base/_Stage_ElementsBase";
-export {ChunkVO} from "./lib/base/ChunkVO";
-export {IMapper} from "./lib/base/IMapper";
-export {MappingMode} from "./lib/base/MappingMode";
+console.debug('AwayJS - Renderer - 0.11.3');
 
-export {DefaultRenderer}				from "./lib/DefaultRenderer";
+export { IRenderEntity } from './lib/base/IRenderEntity';
+export { IMaterial } from './lib/base/IMaterial';
+export { IMaterialClass } from './lib/base/IMaterialClass';
+export { _IRender_ElementsClass } from './lib/base/_IRender_ElementsClass';
+export { _IRender_MaterialClass } from './lib/base/_IRender_MaterialClass';
+export { IPass } from './lib/base/IPass';
+export { ITexture } from './lib/base/ITexture';
+export { IElements } from './lib/base/IElements';
+export { ShaderBase } from './lib/base/ShaderBase';
+export { _IRender_RenderableClass } from './lib/base/_IRender_RenderableClass';
+export { _Render_MaterialBase } from './lib/base/_Render_MaterialBase';
+export { _Render_ElementsBase } from './lib/base/_Render_ElementsBase';
+export { _Render_RenderableBase } from './lib/base/_Render_RenderableBase';
+export { RenderEntity } from './lib/base/RenderEntity';
+export { IAnimationSet } from './lib/base/IAnimationSet';
+export { IAnimator } from './lib/base/IAnimator';
+export { Style } from './lib/base/Style';
+export { _Shader_TextureBase } from './lib/base/_Shader_TextureBase';
+export { AnimationRegisterData } from './lib/base/AnimationRegisterData';
+export { AnimationNodeBase } from './lib/base/AnimationNodeBase';
+export { _Stage_ElementsBase } from './lib/base/_Stage_ElementsBase';
+export { ChunkVO } from './lib/base/ChunkVO';
+export { IMapper } from './lib/base/IMapper';
+export { MappingMode } from './lib/base/MappingMode';
 
-export {DepthRenderer}				from "./lib/DepthRenderer";
+export { DefaultRenderer } from './lib/DefaultRenderer';
 
-export {DistanceRenderer}				from "./lib/DistanceRenderer";
+export { DepthRenderer } from './lib/DepthRenderer';
 
-export {RenderableEvent} from "./lib/events/RenderableEvent";
-export {PassEvent} from "./lib/events/PassEvent";
-export {MaterialEvent} from "./lib/events/MaterialEvent";
-export {StyleEvent} from "./lib/events/StyleEvent";
-export {ElementsEvent}					from "./lib/events/ElementsEvent";
+export { DistanceRenderer } from './lib/DistanceRenderer';
 
-export {Filter3DRenderer}				from "./lib/Filter3DRenderer";
+export { RenderableEvent } from './lib/events/RenderableEvent';
+export { PassEvent } from './lib/events/PassEvent';
+export { MaterialEvent } from './lib/events/MaterialEvent';
+export { StyleEvent } from './lib/events/StyleEvent';
+export { ElementsEvent } from './lib/events/ElementsEvent';
 
-export {ElementsUtils}					from "./lib/utils/ElementsUtils";
-export {MaterialUtils}					from "./lib/utils/MaterialUtils";
+export { Filter3DRenderer } from './lib/Filter3DRenderer';
 
-export {RendererBase}					from "./lib/RendererBase";
-export {RenderGroup, RendererType} from "./lib/RenderGroup";
+export { ElementsUtils } from './lib/utils/ElementsUtils';
+export { MaterialUtils } from './lib/utils/MaterialUtils';
 
-export {IRenderEntitySorter} from "./lib/sort/IRenderEntitySorter";
-export {RenderableMergeSort} from "./lib/sort/RenderableMergeSort";
-export {RenderableNullSort} from "./lib/sort/RenderableNullSort";
+export { RendererBase } from './lib/RendererBase';
+export { RenderGroup, RendererType } from './lib/RenderGroup';
+
+export { IRenderEntitySorter } from './lib/sort/IRenderEntitySorter';
+export { RenderableMergeSort } from './lib/sort/RenderableMergeSort';
+export { RenderableNullSort } from './lib/sort/RenderableNullSort';
+
+export * from './lib/Settings';
