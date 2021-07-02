6400a307343fdef926c83956fed840c1e13e7322
Konstantin Timoshenko <rondo.devil@gmail.com>
2021-07-02 17:47:00 +0300

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-console.debug('AwayJS - Renderer - 0.11.36');
+console.debug('AwayJS - Renderer - 0.11.37');
 
 export { IRenderContainer } from './lib/base/IRenderContainer';
 export { IMaterial } from './lib/base/IMaterial';
