bc1cf87dd53cdb3ebd9d65af1161079f319015fb
Rob Bateman <rob@infiniteturtles.co.uk>
2024-01-26 23:53:48 +0000

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-console.debug('AwayJS - Renderer - 0.11.63');
+console.debug('AwayJS - Renderer - 0.11.64');
 
 export { IRenderContainer } from './lib/base/IRenderContainer';
 export { IMaterial } from './lib/base/IMaterial';
